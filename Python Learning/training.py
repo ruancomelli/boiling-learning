5f913b5ef6759195c1cde8f4d581b6b6b5116cfe
Ruan Comelli <ruancomelli@gmail.com>
2020-05-04 18:43:17 -0300

    Removed unnecessary code

--- Python Learning/training.py
+++ Python Learning/training.py
@@ -1,16 +1,6 @@
 import os
 import pathlib
 
-if False:
-    if os.environ['COMPUTERNAME'] == 'LABSOLAR29-001':
-        os.environ["PATH"] += os.pathsep + str(pathlib.Path('C:') / 'Users' / 'ruan.comelli'/ 'AppData' / 'Local' / 'Continuum' / 'anaconda3')
-        os.environ["PATH"] += os.pathsep + str(pathlib.Path('C:') / 'Users' / 'ruan.comelli'/ 'AppData' / 'Local' / 'Continuum' / 'anaconda3' / 'Library' / 'mingw-w64' / 'bin')
-        os.environ["PATH"] += os.pathsep + str(pathlib.Path('C:') / 'Users' / 'ruan.comelli'/ 'AppData' / 'Local' / 'Continuum' / 'anaconda3' / 'Library' / 'usr' / 'bin')
-        os.environ["PATH"] += os.pathsep + str(pathlib.Path('C:') / 'Users' / 'ruan.comelli'/ 'AppData' / 'Local' / 'Continuum' / 'anaconda3' / 'Library' / 'bin')
-        os.environ["PATH"] += os.pathsep + str(pathlib.Path('C:') / 'Users' / 'ruan.comelli'/ 'AppData' / 'Local' / 'Continuum' / 'anaconda3' / 'Scripts')
-        os.environ["PATH"] += os.pathsep + str(pathlib.Path('C:') / 'Users' / 'ruan.comelli'/ 'AppData' / 'Local' / 'Continuum' / 'anaconda3' / 'bin')
-        os.environ["PATH"] += os.pathsep + str(pathlib.Path('C:') / 'Users' / 'ruan.comelli'/ 'AppData' / 'Local' / 'Continuum' / 'anaconda3' / 'condabin')
-
 import numpy as np
 
 import tensorflow as tf
