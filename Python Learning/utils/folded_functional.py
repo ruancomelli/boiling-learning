6a5b98f03bce101f4fd005fef7151ed0677e79b2
Ruan Comelli <ruancomelli@gmail.com>
2020-05-04 18:46:05 -0300

    Experimental folded_functional

---
+++ Python Learning/utils/folded_functional.py
@@ -0,0 +1,36 @@
+import utils
+# TODO: from folded to packed
+class Folded:
+    def __init__(self, f):
+        self._fun = f
+        
+    def __call__(self, args, kwargs):
+        return self._fun(*args, **kwargs)
+    
+def List(*args):
+    return (
+		args,
+		{}
+    )
+
+def Dict(**kwargs):
+    return (
+		[],
+		kwargs
+	)
+    
+def compose(*ffs, filter_None=False):
+    from functools import reduce
+    
+    def _compose2(f, g):
+        if filter_None:
+            if f is None:
+                f = utils.identity
+            if g is None:
+                g = utils.identity
+        
+        def result(x):
+            return g(f(x))
+        return result
+    
+    return reduce(_compose2, ffs, utils.identity)
