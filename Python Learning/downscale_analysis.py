3150e9282c53583e7c47b2a3979959b85fc18bac
Ruan Comelli <ruancomelli@gmail.com>
2020-05-04 17:43:56 -0300

    Script for analyzing downscaling algorithms

---
+++ Python Learning/downscale_analysis.py
@@ -0,0 +1,89 @@
+def evaluate_downsampling(img, evaluator, downsamplers):
+    return [
+        evaluator(img, downsampler(img))
+        for downsampler in downsamplers
+    ]
+
+def img_variance(img):
+    from numpy import var
+
+    return var(img, axis=(0, 1))
+
+def img_retained_variance(ref, img):
+    return (
+        img_variance(img)
+        / img_variance(ref)
+    )
+
+def img_shannon_cross_entropy(ref, img):
+    pass
+
+def img_shannon_cross_entropy_ratio(ref, img):
+    return (
+        img_shannon_cross_entropy(ref, img)
+        / img_shannon_cross_entropy(ref, ref)
+    )
+
+from skimage.io import imread
+from more_itertools import first
+from functools import partial
+
+from skimage.color import rgb2gray
+
+img_path = first((case.path / 'frames_crop').glob('**/*.png'))
+print(img_path)
+img = rgb2gray(imread(img_path))
+
+ev_ds = evaluate_downsampling(
+    img,
+    img_retained_variance,
+    [
+        partial(utils.image.downscale, shape=ds)
+        for ds in range(1, 11)
+    ]
+)
+print(ev_ds)
+
+# ------------------------------------------
+# PART 2
+# ------------------------------------------
+
+from skimage.io import imshow, imread
+from skimage import img_as_float
+import matplotlib.pyplot as plt
+
+import utils
+
+in_path = python_project_home_path / 'testing_extract' / 'from_0_to_9' / 'my_frame_1.png'
+
+plt.subplot(1, 3, 1)
+
+img = imread(in_path)
+imshow(img)
+
+top = 600
+bottom = 230
+left = 1000
+right = 1100
+
+plt.subplot(1, 3, 2)
+
+cropped = utils.image.crop(
+    img,
+    top=top,
+    bottom=bottom,
+    left=left,
+    right=right,
+)
+print(cropped.shape)
+imshow(cropped)
+
+plt.subplot(1, 3, 3)
+
+img = img_as_float(img)
+downscaled = utils.image.downscale(
+    img,
+    5
+)
+print(downscaled.shape)
+imshow(downscaled)
