f39ae0695b68b68107a9876e6080bf565e01a112
Ruan Comelli <ruancomelli@gmail.com>
2020-05-27 10:36:26 -0300

    TO-DO list updated; Mirror archived

---
+++ Python Learning/archive/using_mirror.py
@@ -0,0 +1,203 @@
+from functools import partial
+
+import tensorflow_addons as tfa
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import load_model
+
+from boiling_learning import model
+from boiling_learning import management
+from boiling_learning.management import Mirror
+from boiling_learning.model_definitions import (
+    HoboldNet1,
+    HoboldNet2,
+    HoboldNet3,
+    HoboldNetSupplementary,
+    KramerNet,
+)
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4, width=160)
+
+n_train = n_val = 1000
+IMG_SHAPE = [224, 224, 1]
+BATCH_SIZE = 1028
+
+grid_dict = {
+    'lr': [1e-2, 1e-3, 1e-4],
+    'reduce_lr_on_plateau_factor': [0.5, 0.2, 0.1],
+    'reduce_lr_on_plateau_patience': [2, 4, 8],
+    'dropout_ratio': [0.2, 0.5, 0.8],
+}
+grid = list(
+    bl.utils.combine_dict(
+        grid_dict,
+        partial(bl.utils.alternate_iter, default_indices=[1, 1, 1, 1])
+    )
+)
+for params in grid:
+    lr = params['lr']
+    reduce_lr_on_plateau_factor = params['reduce_lr_on_plateau_factor']
+    reduce_lr_on_plateau_patience = params['reduce_lr_on_plateau_patience']
+    dropout_ratio = params['dropout_ratio']
+
+    model_type = HoboldNet2
+    optimizer_params = dict(
+        lr=lr,
+    )
+    reduce_lr_on_plateau_params = dict(
+        monitor='val_loss', 
+        factor=reduce_lr_on_plateau_factor,
+        patience=reduce_lr_on_plateau_patience,
+        min_delta=0.0001, min_lr=0, 
+        verbose=1, mode='auto', cooldown=0
+    )
+    early_stopping_params = dict(
+        monitor='val_loss', 
+        min_delta=0, patience=10, baseline=None,
+        verbose=1, mode='auto',
+        restore_best_weights=True
+    )
+    best_validation_callback_file_name = 'BestValidation_epoch{epoch}'
+    best_validation_callback_params = dict(
+        filepath=best_validation_callback_file_name,
+        save_best_only=True,
+        monitor='val_loss',
+    )
+    last_trained_callback_file_name = 'LastTrained_epoch{epoch}'
+    last_trained_callback_params = dict(
+        filepath=last_trained_callback_file_name,
+        save_best_only=False,
+        monitor='val_loss',
+    )
+    checkpoint_params = dict(
+        restore=True,
+        path=last_trained_callback_file_name,
+    )
+
+    model_params = Mirror({'propagate': True},
+        fetch=Mirror.Fork(
+            value=['model', 'history']
+        ),
+        verbose=Mirror.Fork(
+            value=1
+        ),
+        checkpoint=Mirror.Fork(
+            desc=checkpoint_params,
+            value=dict(checkpoint_params),
+        ),
+        input_shape=IMG_SHAPE,
+        # num_classes=3,
+        dropout_ratio=dropout_ratio,
+        problem='regression',
+        compile_setup=Mirror({'propagate': True},
+            do=True,
+            params=Mirror({'propagate': True},
+                optimizer=Mirror.Fork(
+                    desc={
+                        'name': 'adam',
+                        'params': optimizer_params
+                    }, 
+                    value=Adam(**optimizer_params)
+                ),
+                loss=Mirror.Fork(
+                    desc='mean_squared_error',
+                    value=tf.keras.losses.MeanSquaredError(),
+                ),
+                metrics=Mirror.Fork(
+                    value=[
+                        tf.keras.metrics.MeanSquaredError('MSE'),
+                        tf.keras.metrics.RootMeanSquaredError('RMS'),
+                        tf.keras.metrics.MeanAbsoluteError('MAE'),
+                        tf.keras.metrics.MeanAbsolutePercentageError('MAPE'),
+                        tfa.metrics.RSquare('R2'),
+                    ]
+                ),
+            )
+        ),
+        fit_setup=Mirror({'propagate': True},
+            do=True,
+            params=Mirror({'propagate': True},
+                x=Mirror.Fork(
+                    desc={
+                        'source': Path('my_data') / 'path',
+                        'train_size': n_train,
+                    },
+                ),
+                validation_data=Mirror.Fork(
+                    desc={
+                        'source': Path('my_data') / 'path',
+                        'val_size': n_val
+                    },
+                ),
+                verbose=Mirror.Fork(
+                    value=2
+                ),
+                batch_size=BATCH_SIZE,
+                epochs=100,
+                use_multiprocessing=True,
+                workers=-1,
+                callbacks=Mirror.Fork(
+                    desc=[
+                        {
+                            'name': 'ModelCheckpoint',
+                            'params': best_validation_callback_params
+                        },
+                        {
+                            'name': 'ModelCheckpoint',
+                            'params': last_trained_callback_params
+                        },
+                        {
+                            'name': 'EarlyStopping',
+                            'params': early_stopping_params
+                        },
+                        {
+                            'name': 'ReduceLROnPlateau',
+                            'params': reduce_lr_on_plateau_params
+                        }
+                    ]
+                )
+            )
+        )
+    )
+    params_desc = model_params['desc']
+    params_value = model_params['value']
+
+    model_path = Path('my_model') / 'path'
+
+    # This part is separated because ModelCheckpoint needs model_path
+    best_validation_callback_path = str(model_path / best_validation_callback_file_name)
+    best_validation_callback = ModelCheckpoint(**bl.utils.merge_dicts(
+        best_validation_callback_params,
+        dict(
+            filepath=best_validation_callback_path,
+            verbose=1
+        )
+    ))
+    last_trained_callback_path = str(model_path / last_trained_callback_file_name)
+    last_trained_callback = ModelCheckpoint(**bl.utils.merge_dicts(
+        last_trained_callback_params,
+        dict(
+            filepath=last_trained_callback_path,
+            verbose=1
+        )
+    ))
+    early_stopping_callback = EarlyStopping(**early_stopping_params)
+    reduce_lr_on_plateau_callback = ReduceLROnPlateau(**reduce_lr_on_plateau_params)
+
+    params_value['fit_setup']['params']['callbacks'] = [
+        best_validation_callback,
+        last_trained_callback,
+        early_stopping_callback,
+        reduce_lr_on_plateau_callback,
+    ]
+    
+    params_value['checkpoint']['path'] = last_trained_callback_path
+    params_value['checkpoint']['load_method'] = load_model
+
+    bl.utils.print_header('Description')
+    pprint.pprint(params_desc)
+    bl.utils.print_header('Value')
+    pprint.pprint(params_value)
+
+    break
--- TODO.md
+++ TODO.md
@@ -6,21 +6,22 @@
   - use the `operator` library;
   - try to remove unnecessary functions;
   - try to remove unnecessary dependencies;
-- [ ] Write unit tests
-- [ ] Write a config file, so that configurations are not hard-coded in Python, but in a JSON file
+- [ ] Write unit tests.
+- [ ] Write a config file, so that configurations are not hard-coded in Python, but in a JSON file.
 - [ ] Python Learning: design a preprocessing function that takes a `tf.tensor`. This function should take a batch, preprocess it (possibly using many cores) and then fetch the results. The results should then be saved to disk. Useful links: [TensorFlow guide to data performance](https://www.tensorflow.org/guide/data_performance), [TensorFlow tutorial to image classification](https://www.tensorflow.org/tutorials/images/classification), [TensorFlow tutorial to loading images](https://www.tensorflow.org/tutorials/load_data/images), [TensorFlow guide to building input pipelines](https://www.tensorflow.org/guide/data).
-- [x] Move library code to a specific module
-- [ ] Improve the project structure
-- [ ] Implement parallelization for `TransformationPipeline`s
-- [ ] Use type annotations where applicable
+- [x] Move library code to a specific module.
+- [ ] Improve the project structure.
+- [ ] Implement parallelization for `TransformationPipeline`s.
+- [ ] Use type annotations where applicable.
 - [ ] Can wrappers remove code repetition in crop and shift for the `if image is None: image = imread(in_path)` and the `if out_path is not None: imsave(out_path)`?
-- [ ] Document code
-- [ ] Check for inconsistent input values in many functions, some of which are marked with a comment like `# TODO: use check_value_match`
-- [ ] In the `Case` class, many properties can be converted to cached properties. Python 3.8 may be necessary
-- [ ] Allow the user to choose if only the train data must be modified `ImageDatasetTransformer`
-- [ ] Allow dataset augmentation, not only transformation in `ImageDatasetTransformer`
-- [ ] In boiling_learning.model.restore: allow a complete dictionary of important keys, not only epoch. For instance, `keys={'epoch_str': int, 'val_acc': float}`
-- [ ] Implement asynchronous `UserPool`
+- [ ] Implement general function dispatching?
+- [ ] Document code.
+- [ ] Check for inconsistent input values in many functions, some of which are marked with a comment like `# TODO: use check_value_match`.
+- [ ] In the `Case` class, many properties can be converted to cached properties. Python 3.8 may be necessary.
+- [ ] Allow the user to choose if only the train data must be modified `ImageDatasetTransformer`.
+- [ ] Allow dataset augmentation, not only transformation in `ImageDatasetTransformer`.
+- [ ] In boiling_learning.model.restore: allow a complete dictionary of important keys, not only epoch. For instance, `keys={'epoch_str': int, 'val_acc': float}`.
+- [ ] Implement asynchronous `UserPool`.
 - [x] In the `Parameters` class, allow getting `dict` keys. Usage: getting from a `dict` creates a branch:
 
 ```python
@@ -39,19 +40,19 @@ assert p == Parameters(params={'a': 1000, 'b': 1, 'd': 2000})
 - [ ] In the `Parameters` class, decide what to do if there are `set` keys inside `list` keys when getting.
 - [ ] In the `Parameters` class, decide what to do if there are `dict` keys inside `list` keys.
 - [ ] In the `Parameters` class, allow modified `del` paths. For instance, `del p[['a', 'b']]` should delete `'b'` from `p['a']`.
-- [ ] Fix apparent error in `utils.remove_duplicates`
+- [ ] Fix apparent error in `utils.remove_duplicates`.
 - [x] Refactor `ModelManager`, including separate functions to:
   - [x] create new model path;
   - [x] retrieve existing model path;
   - [x] check if model path already exists;
   - [x] check if model was already saved, or if it exists only in the table;
-- [ ] Format code in PEP style
-- [ ] Decide if `Parameters` will supporting forking
+- [ ] Format code in PEP style.
+- [ ] Decide if `Parameters` will supporting forking.
 - [ ] Use [TensorFlow estimators](https://www.tensorflow.org/guide/estimator)?
-- [ ] Allow different batch sizes for different models
+- [x] Allow different batch sizes for different models.
 - [ ] Why do `more_itertools.filter_except` and `more_itertools.map_except` need to do `exceptions = tuple(exceptions)`?
-- [ ] Finish step detection analysis
-- [ ] Implement a mini-translator: translates from the old-string-format minilanguage to the new format
+- [ ] Finish step detection analysis.
+- [ ] Implement a mini-translator: translates from the old-string-format minilanguage to the new format.
 - [ ] Implement a function wrapper that transforms the arguments before forwarding. For instance: 
 
 ```python
@@ -60,3 +61,5 @@ lower_eq = transform(operator.eq, keyfunc=lambda x: x.lower())
 assert 'Hi' != 'hi'
 assert lower_eq('Hi', 'hi')
 ```
+
+- [ ] Why is there a `more_itertools.prepend`, but not a `more_itertools.append`?
