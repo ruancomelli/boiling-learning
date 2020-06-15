95f9b0188aa618374934f0d895274e7d0e90b883
Ruan Comelli <ruancomelli@gmail.com>
2020-06-15 15:41:54 -0300

    Refactor frame extraction and iteration

--- Python Learning/boiling_learning/preprocessing/video.py
+++ Python Learning/boiling_learning/preprocessing/video.py
@@ -2,101 +2,105 @@ import contextlib
 import subprocess
 import os
 import binascii
+import operator
+import tempfile
 from pathlib import Path
         
 import cv2
 from more_itertools import ilen, unzip
 from parse import parse
 
+import boiling_learning as bl
+
 # Original code: $ ffmpeg -i "video.mov" -f image2 "video-frame%05d.png"
 # Source 2: <https://forums.fast.ai/t/extracting-frames-from-video-file-with-ffmpeg/29818>
-def extract_frames(filepath, outputdir, filename_pattern='frame%d.png', frame_list=None, verbose=False, make_parents=True, final_pattern=None):
+def extract_frames(
+    filepath,
+    outputdir,
+    filename_pattern='frame%d.png',
+    verbose=False,
+    final_pattern=None,
+    fast_frames_count=None,
+    iterate=False,
+    overwrite=False
+):
     # TODO: use check_value_match?
     # TODO: raise for invalid options
-    # TODO: when creating unique temporary folder, check that it can't match any generated subfolder
-    # TODO: use python's tempfile module
     
-    if frame_list is not None or final_pattern is not None:
-        
-        if frame_list is not None:
-            frame_list = sorted(frame_list, key=lambda x: x[0])
-            
-            if make_parents:
-                outputdir.mkdir(exist_ok=True, parents=True)
-            
-            frame_indices, filenames = unzip(frame_list)
-            frame_indices, filenames = list(int(x) for x in frame_indices), list(filenames)
-                
-            frames_used = set()
-            for instance in frame_indices:
-                while instance in frames_used:
-                    instance += 1
-                frames_used.add(instance)
-                
-            portion = 'select=\'' + '+'.join(f'eq(n\,{frame_index})' for frame_index in frames_used) + '\''
-            
-            if verbose:
-                print(f'portion = {portion}')
-                
-        if isinstance(final_pattern, str):
-            str_final_pattern = final_pattern
-            final_pattern = lambda index: str_final_pattern.format(index=index)
-            
-        temporary_folder = outputdir / binascii.b2a_hex(os.urandom(5)).decode()
-        while temporary_folder.is_dir():
-            temporary_folder = outputdir / binascii.b2a_hex(os.urandom(5)).decode()
-        temporary_folder.mkdir(parents=True)
-        
-        intermediate_format, intermediate_parser = tuple(
-            f'frame{ref}{Path(filename_pattern).suffix}'
-            for ref in ('%d', '{index:d}')
-        )
-        
-        command_list =  [
-            'ffmpeg',
-            '-i',
-            str(filepath),
-            '-qscale:v',
-            '1'
-        ] + (
-            [
-                '-vf',
-                portion,
-            ]
-            if frame_list is not None
-            else []
-        ) + [
-            '-vsync',
-            '0',
-            str(temporary_folder / intermediate_format)
-        ]
-        
-        if verbose:
-            print(f'command list = {command_list}')
-        
-        subprocess.run(command_list)
+    if fast_frames_count is not None and overwrite:
+        raise ValueError('cannot overwrite when frames count mode is passed.')
+    
+    bl.utils.print_verbose(verbose, f'Extracting frames from {filepath} to {outputdir}')
+    
+    filepath = Path(filepath)
+    outputdir = Path(outputdir)
+    frame_suffix = Path(filename_pattern).suffix 
 
-        if final_pattern is not None:
+    if fast_frames_count is not None and outputdir.is_dir():
+        video_frames_count = count_frames(filepath, fast=fast_frames_count)
+        extracted_frames_count = ilen(outputdir.rglob(f'*{frame_suffix}'))
+        
+        bl.utils.print_verbose(verbose, f'Frames extracted: {extracted_frames_count}/{video_frames_count}')
+        
+        if video_frames_count == extracted_frames_count:
+            bl.utils.print_verbose(verbose, 'Frames already extracted. Skipping.')
+            return
+    
+    outputdir.mkdir(exist_ok=True, parents=True)
+    
+    if final_pattern is not None:
+        if isinstance(final_pattern, str):
+            formatter = final_pattern.format
+            
+            def final_pattern(index):
+                return formatter(index=index)                
+            
+    if iterate: # TODO: test!!!
+        for index, frame in enumerate(frames(filepath)):
+            path = outputdir / final_pattern(index)
+
+            if not overwrite and path.is_file():
+                continue
+
+            path.parent.mkdir(exist_ok=True, parents=True)            
+            cv2.imwrite(str(path), frame)
+            
+    elif final_pattern is not None:
+        with bl.utils.tempdir(prefix='_', dir=outputdir) as temporary_folder:
+            intermediate_format = f'frame%d{frame_suffix}'
+            intermediate_parser = f'frame{{index:d}}{frame_suffix}'
+            
+            command_list =  [
+                'ffmpeg',
+                '-i',
+                str(filepath),
+                '-qscale:v',
+                '1',
+                '-vsync',
+                '0',
+                str(temporary_folder / intermediate_format)
+            ]
+            
+            bl.utils.print_verbose(verbose, f'command list = {command_list}')
+            
+            subprocess.run(command_list)
+
             source_dest_pairs = (
                 (source, outputdir / final_pattern(parse(intermediate_parser, source.name)['index']))
                 for source in temporary_folder.iterdir()
             )
-        else:
-            source_dest_pairs = (
-                (temporary_folder / (intermediate_format % i), outputdir / filename)
-                for i, filename in enumerate(filenames, start=1)
-            )
-        source_dest_pairs = filter(lambda source_dest_pair: source_dest_pair[0].is_file(), source_dest_pairs)
             
-        for source, dest in source_dest_pairs:            
-            if make_parents:
-                dest.parent.mkdir(exist_ok=True, parents=True)
-            source.rename(dest)
+            source_dest_pairs = filter(lambda source_dest_pair: source_dest_pair[0].is_file(), source_dest_pairs)
+            
+            if not overwrite:
+                source_dest_pairs = filter(lambda source_dest_pair: not source_dest_pair[1].is_file(), source_dest_pairs)
                 
-        temporary_folder.rmdir()
+            for source, dest in source_dest_pairs:            
+                dest.parent.mkdir(exist_ok=True, parents=True)
+                source.rename(dest)
     else:
-        if make_parents:
-            outputdir.mkdir(exist_ok=True, parents=True)
+        if not overwrite:
+            raise ValueError('when not all frames are extracted, final_pattern is not used and not in iterable mode, I have to overwrite.')
         
         command_list =  [
             'ffmpeg',
@@ -169,11 +173,14 @@ def open_video(video_path):
         cap.release()
         
 # Does not work with GOPRO format
-def frames(video_path):
+def frames(video_path, suppress_retrieval_failure=True):
     with open_video(video_path) as cap:
         while cap.grab():
             flag, frame = cap.retrieve()
-            yield frame
+            if flag:
+                yield frame
+            elif not suppress_retrieval_failure:
+                raise RuntimeError(f'failed frame retrieval for video at {video_path}')
             
 def count_frames(video_path, fast=False):
     if fast:
