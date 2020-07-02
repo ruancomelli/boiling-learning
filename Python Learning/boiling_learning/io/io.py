03ae91feb949b6cc98844d846af9453ae3b26f5d
Ruan Comelli <ruancomelli@gmail.com>
2020-07-02 17:42:20 -0300

    Use new `bl.utils.ensure_*` functions in `bl.io`

--- Python Learning/boiling_learning/io/io.py
+++ Python Learning/boiling_learning/io/io.py
@@ -5,33 +5,33 @@ import pickle
 import h5py
 from tensorflow.keras.models import load_model
 
-from boiling_learning.utils import nullcontext
+from boiling_learning.utils import (
+    nullcontext,
+    ensure_resolved,
+    ensure_parent
+)
 
 def save_serialized(save_map):
     def save(return_dict, path):
-        path = Path(path)
-        path.mkdir(exist_ok=True, parents=True)
+        path = ensure_parent(path)
         for key, obj in return_dict.items():
             path_ = path / key
             save_map[key](obj, path_)
     return save
 
 def save_keras_model(keras_model, path, **kwargs):
-    path = Path(path).absolute().resolve()
-    path.parent.mkdir(exist_ok=True, parents=True)
+    path = ensure_parent(path)
     
     keras_model.save(path, **kwargs)
 
 def save_pkl(obj, path):
-    path = Path(path).absolute().resolve()
-    path.parent.mkdir(exist_ok=True, parents=True)
+    path = ensure_parent(path)
 
     with path.open('wb') as file:
         pickle.dump(obj, file, protocol=pickle.HIGHEST_PROTOCOL)
         
 def save_json(obj, path):
-    path = Path(path).absolute().resolve()
-    path.parent.mkdir(exist_ok=True, parents=True)
+    path = ensure_parent(path)
 
     with path.open('w', encoding='utf-8') as file:
         json.dump(obj, file, indent=4, ensure_ascii=False)
@@ -57,23 +57,23 @@ def load_keras_model(path, strategy=None, **kwargs):
         return load_model(path, **kwargs)
 
 def load_pkl(path):
-    with Path(path).absolute().resolve().open('rb') as file:
+    with ensure_resolved(path).open('rb') as file:
         return pickle.load(file)
 
 def load_json(path):
-    with Path(path).absolute().resolve().open('r', encoding='utf-8') as file:
+    with ensure_resolved(path).open('r', encoding='utf-8') as file:
         return json.load(file)
 
 def saver_hdf5(key=''):
     def save_hdf5(obj, path):
-        path = Path(path).absolute().resolve()
+        path = ensure_parent(path)
         with h5py.File(str(path), 'w') as hf:
             hf.create_dataset(key, data=obj)
     return save_hdf5
 
 def loader_hdf5(key=''):
     def load_hdf5(path):
-        path = Path(path).absolute().resolve()
+        path = ensure_resolved(path)
         with h5py.File(str(path), 'r') as hf:
             return hf.get(key)
     return load_hdf5
