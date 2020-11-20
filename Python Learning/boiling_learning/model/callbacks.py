bef61b75bf9f6078d4ef5e3296ffe2bb7603b941
Ruan Comelli <ruancomelli@gmail.com>
2020-11-20 16:57:44 -0300

    Improve Keras's `ReduceLROnPlateau` by allowing relative `min_delta`s

--- Python Learning/boiling_learning/model/callbacks.py
+++ Python Learning/boiling_learning/model/callbacks.py
@@ -1,6 +1,10 @@
 import datetime
 
+import numpy as np
 from tensorflow.keras.callbacks import Callback
+from tensorflow.python.keras import backend as K
+from tensorflow.python.platform import tf_logging as logging
+
 
 # Source: <https://stackoverflow.com/q/47731935/5811400>
 class AdditionalValidationSets(Callback):
@@ -17,17 +21,17 @@ class AdditionalValidationSets(Callback):
         batch size to be used when evaluating on the additional datasets
         """
         super().__init__()
-        
+
         self.validation_sets = [
             self._expand_validation_set(val_set, idx)
             for idx, val_set in enumerate(validation_sets)
         ]
-            
+
         self.epoch = []
         self.history = {}
         self.verbose = verbose
         self.batch_size = batch_size
-        
+
     def _expand_validation_set(self, val_set, idx):
         if len(val_set) == 1:
             return val_set[0], None, None, f'val_{idx}'
@@ -38,7 +42,9 @@ class AdditionalValidationSets(Callback):
         elif len(val_set) == 4:
             return val_set
         else:
-            raise ValueError('every validation set must be either in the form (data,), (data, name), (x, y, name) or (x, y, sample_weights, name).')
+            raise ValueError(
+                'every validation set must be either in the form'
+                ' (data,), (data, name), (x, y, name) or (x, y, sample_weights, name).')
 
     def on_train_begin(self, logs=None):
         self.epoch = []
@@ -56,26 +62,29 @@ class AdditionalValidationSets(Callback):
         for validation_set in self.validation_sets:
             validation_data, validation_targets, sample_weights, validation_set_name = validation_set
 
-            results = self.model.evaluate(x=validation_data,
-                                          y=validation_targets,
-                                          verbose=self.verbose,
-                                          sample_weight=sample_weights,
-                                          batch_size=self.batch_size)
-            
+            results = self.model.evaluate(
+                x=validation_data,
+                y=validation_targets,
+                verbose=self.verbose,
+                sample_weight=sample_weights,
+                batch_size=self.batch_size
+            )
+
             names = ['loss'] + [m.name for m in self.model.metrics]
             full_names = [validation_set_name + '_' + name for name in names]
             full_results = [logs['loss']] + results
 
             for full_name, result in zip(full_names, full_results):
                 self.history.setdefault(full_name, []).append(result)
-                
+
             if self.verbose >= 1:
                 values_str = ' - '.join([
                     f'{name}: {result}'
                     for name, result in zip(names, full_results)
                 ])
                 print(f'{validation_set_name}[{values_str}]')
-                
+
+
 class TimePrinter(Callback):
     def __init__(
         self,
@@ -84,10 +93,10 @@ class TimePrinter(Callback):
         when=None,
     ):
         super().__init__()
-                
+
         self.streamer = streamer
         self.fmt = fmt
-        
+
         if when is None:
             when = {
                 'on_batch_begin',
@@ -108,10 +117,10 @@ class TimePrinter(Callback):
                 'on_train_end',
             }
         self.when = when
-        
+
     def _str_now(self):
         return datetime.datetime.now().strftime(self.fmt)
-    
+
     def on_batch_begin(self, *args, **kwargs):
         if 'on_batch_begin' in self.when:
             self.streamer(f'--- beginning batch at {self._str_now()}', end='')
@@ -176,4 +185,133 @@ class TimePrinter(Callback):
         if 'on_train_end' in self.when:
             self.streamer(f'- ending train at {self._str_now()}')
 
-        
+
+class ReduceLROnPlateau(Callback):
+    """Reduce learning rate when a metric has stopped improving.
+    Models often benefit from reducing the learning rate by a factor
+    of 2-10 once learning stagnates. This callback monitors a
+    quantity and if no improvement is seen for a 'patience' number
+    of epochs, the learning rate is reduced.
+    Example:
+    ```python
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2,
+                                patience=5, min_lr=0.001)
+    model.fit(X_train, Y_train, callbacks=[reduce_lr])
+    ```
+    Arguments:
+        monitor: quantity to be monitored.
+        factor: factor by which the learning rate will be reduced.
+        `new_lr = lr * factor`.
+        patience: number of epochs with no improvement after which learning rate
+        will be reduced.
+        verbose: int. 0: quiet, 1: update messages.
+        mode: one of `{'auto', 'min', 'max'}`. In `'min'` mode,
+        the learning rate will be reduced when the
+        quantity monitored has stopped decreasing; in `'max'` mode it will be
+        reduced when the quantity monitored has stopped increasing; in `'auto'`
+        mode, the direction is automatically inferred from the name of the
+        monitored quantity.
+        min_delta: threshold for measuring the new optimum, to only focus on
+        significant changes.
+        min_delta_mode: one of `{'absolute', 'relative'}`.
+        cooldown: number of epochs to wait before resuming normal operation after
+        lr has been reduced.
+        min_lr: lower bound on the learning rate.
+    """
+
+    def __init__(
+            self,
+            monitor='val_loss',
+            factor=0.1,
+            patience=10,
+            verbose=0,
+            mode='auto',
+            min_delta=1e-4,
+            min_delta_mode='absolute',
+            cooldown=0,
+            min_lr=0
+    ):
+        super(ReduceLROnPlateau, self).__init__()
+
+        self.monitor = monitor
+        if factor >= 1.0:
+            raise ValueError('ReduceLROnPlateau does not support a factor >= 1.0.')
+        self.factor = factor
+        self.min_lr = min_lr
+        self.min_delta = min_delta
+        self.min_delta_mode = min_delta_mode
+        self.patience = patience
+        self.verbose = verbose
+        self.cooldown = cooldown
+        self.cooldown_counter = 0  # Cooldown counter.
+        self.wait = 0
+        self.best = 0
+        self.mode = mode
+        self.monitor_op = None
+        self._reset()
+
+    def _reset(self):
+        """Resets wait counter and cooldown counter.
+        """
+        if self.mode not in {'auto', 'min', 'max'}:
+            logging.warning('Learning rate reduction mode %s is unknown, '
+                            'fallback to auto mode.', self.mode)
+            self.mode = 'auto'
+
+        if self.min_delta_mode not in {'absolute', 'relative'}:
+            logging.warning('Minimum delta mode %s is unknown, '
+                            'fallback to absolute mode.', self.min_delta_mode)
+            self.min_delta_mode = 'absolute'
+
+        if (self.mode == 'min' or (self.mode == 'auto' and 'acc' not in self.monitor)):
+            if self.min_delta_mode == 'relative':
+                self.monitor_op = lambda current, best: np.less(current, (1 - self.min_delta)*best)
+            else:
+                self.monitor_op = lambda current, best: np.less(current, best - self.min_delta)
+            self.best = np.Inf
+        else:
+            if self.min_delta_mode == 'relative':
+                self.monitor_op = lambda current, best: np.greater(current, (1 + self.min_delta)*best)
+            else:
+                self.monitor_op = lambda current, best: np.greater(current, best + self.min_delta)
+            self.best = -np.Inf
+        self.cooldown_counter = 0
+        self.wait = 0
+
+    def on_train_begin(self, logs=None):
+        self._reset()
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        logs['lr'] = K.get_value(self.model.optimizer.lr)
+        current = logs.get(self.monitor)
+        if current is None:
+            logging.warning('Learning rate reduction is conditioned on metric `%s` '
+                            'which is not available. Available metrics are: %s',
+                            self.monitor, ', '.join(logs.keys()))
+
+        else:
+            if self.in_cooldown():
+                self.cooldown_counter -= 1
+                self.wait = 0
+
+            if self.monitor_op(current, self.best):
+                self.best = current
+                self.wait = 0
+            elif not self.in_cooldown():
+                self.wait += 1
+            if self.wait >= self.patience:
+                old_lr = K.get_value(self.model.optimizer.lr)
+                if old_lr > np.float32(self.min_lr):
+                    new_lr = old_lr * self.factor
+                    new_lr = max(new_lr, self.min_lr)
+                    K.set_value(self.model.optimizer.lr, new_lr)
+                if self.verbose > 0:
+                    print(
+                        f'\nEpoch {epoch+1:05d}: ReduceLROnPlateau reducing learning rate to {new_lr}.'
+                    )
+                self.cooldown_counter = self.cooldown
+                self.wait = 0
+
+    def in_cooldown(self):
+        return self.cooldown_counter > 0
