a05b28c3c9cd85c19bf208930e547e7b8c14d6a9
Ruan Comelli <ruancomelli@gmail.com>
2021-04-15 10:10:50 -0300

    Remove unnecessary dependency on scikit-learn

--- boiling_learning/datasets/datasets.py
+++ boiling_learning/datasets/datasets.py
@@ -8,7 +8,6 @@ from typing import Callable, Iterable, Optional, Union
 import funcy
 import more_itertools as mit
 import tensorflow as tf
-from sklearn.model_selection import train_test_split
 
 import boiling_learning.utils.mathutils as mathutils
 from boiling_learning.io.io import DatasetTriplet
@@ -100,61 +99,6 @@ SplitSubset.FROM_STR = {
     SplitSubset.TEST: {'test'},
     SplitSubset.ALL: {'all'},
 }
-
-
-def train_val_test_split(
-        dataset,
-        n_samples,
-        train_size=None,
-        val_size=None,
-        test_size=None,
-        **options
-):
-    # TODO: this function only accepts one dataset. Allow more.
-    # TODO: this function requires the argument n_samples. Remove this.
-    # TODO: to keep consistency, allow elements from SplitSubset.FROM_STR
-
-    if val_size is None or val_size == 0:
-        train_set, test_set = train_test_split(
-            dataset,
-            train_size=train_size,
-            test_size=test_size,
-            **options
-        )
-        val_set = []
-    else:
-        if 0 < val_size < 1:
-            val_size = int(val_size * n_samples)
-        elif val_size < 0 or val_size > n_samples:
-            raise ValueError(
-                f'invalid val_size {val_size}.'
-                'Expected a float in (0, 1),'
-                f'or a float in [0, n_samples={n_samples}].')
-
-        if train_size is None:
-            train_set, test_set = train_test_split(
-                dataset,
-                test_size=test_size,
-                **options
-            )
-            train_set, val_set = train_test_split(
-                train_set,
-                test_size=val_size,
-                **options
-            )
-        else:
-            train_set, test_set = train_test_split(
-                dataset,
-                train_size=train_size,
-                **options
-            )
-            val_set, test_set = train_test_split(
-                test_set,
-                train_size=val_size,
-                **options
-            )
-
-    return train_set, val_set, test_set
 
 
 def tf_concatenate(datasets: Iterable[tf.data.Dataset]) -> tf.data.Dataset:
