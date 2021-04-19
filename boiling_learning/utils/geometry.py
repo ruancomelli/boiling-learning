edc766581def42efd4e0221bd85dcc9d60e576f4
Ruan Comelli <ruancomelli@gmail.com>
2021-04-19 11:57:26 -0300

    Definitely fix bugs in geometry module

--- boiling_learning/utils/geometry.py
+++ boiling_learning/utils/geometry.py
@@ -3,22 +3,19 @@ import pint
 from dataclassy import dataclass
 
 
-@dataclass(frozen=True, kwargs=True)
+@dataclass(kwargs=True)
 class Solid:
     surface_area: pint.Quantity = None
     volume: pint.Quantity = None
 
 
-@dataclass(frozen=True, kwargs=True)
 class Prism(Solid):
     length: pint.Quantity
 
-    cross_section_perimeter: pint.Quantity = None
-    cross_section_area: pint.Quantity = None
+    cross_section_perimeter: pint.Quantity
+    cross_section_area: pint.Quantity
 
     lateral_area: pint.Quantity = None
-    surface_area: pint.Quantity = None
-    volume: pint.Quantity = None
 
     def __init__(self) -> None:
         self.lateral_area = self.cross_section_perimeter * self.length
@@ -26,16 +23,16 @@ class Prism(Solid):
         self.volume = self.cross_section_area * self.length
 
 
-@dataclass(frozen=True, kwargs=True)
 class Cylinder(Prism):
     diameter: pint.Quantity
+
+    cross_section_perimeter: pint.Quantity = None
+    cross_section_area: pint.Quantity = None
 
     def __init__(self) -> None:
         self.radius = self.diameter / 2
         self.cross_section_perimeter = np.pi * self.diameter
         self.cross_section_area = np.pi * self.radius**2
-
-        super().__init__()
 
 
 @dataclass(frozen=True, kwargs=True)
@@ -43,8 +40,9 @@ class RectangularPrism(Prism):
     width: pint.Quantity
     thickness: pint.Quantity
 
+    cross_section_perimeter: pint.Quantity = None
+    cross_section_area: pint.Quantity = None
+
     def __init__(self) -> None:
         self.cross_section_perimeter = 2*(self.width + self.thickness)
         self.cross_section_area = self.width * self.thickness
-
-        super().__init__()
