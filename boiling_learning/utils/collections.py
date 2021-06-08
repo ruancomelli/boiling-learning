d2e374c3240d76b478c2ea4c542829c79f6b8369
Ruan Comelli <ruancomelli@gmail.com>
2021-06-08 14:57:57 -0300

    Implement KeyedSet collection: a Set whose items can be retrieved by keys

---
+++ boiling_learning/utils/collections.py
@@ -0,0 +1,42 @@
+from typing import (Callable, Dict, Generic, Hashable, Iterable, Iterator,
+                    KeysView, MutableSet, TypeVar, ValuesView)
+
+_Key = TypeVar('_Key', bound=Hashable)
+_Value = TypeVar('_Value')
+
+
+class KeyedSet(MutableSet[_Value], Generic[_Key, _Value]):
+    def __init__(
+            self,
+            key: Callable[[_Value], _Key],
+            iterable: Iterable[_Value] = ()
+    ) -> None:
+        self.__key: Callable[[_Value], _Key] = key
+        self.__data: Dict[_Key, _Value] = {
+            self.__key(element): element
+            for element in iterable
+        }
+
+    def keys(self) -> KeysView:
+        return self.__data.keys()
+
+    def values(self) -> ValuesView:
+        return self.__data.values()
+
+    def __len__(self) -> int:
+        return len(self.__data)
+
+    def __getitem__(self, key: _Key) -> _Value:
+        return self.__data[key]
+
+    def __contains__(self, element: _Value) -> bool:
+        return element in self.values()
+
+    def __iter__(self) -> Iterator[_Value]:
+        return iter(self.values())
+
+    def add(self, element: _Value) -> None:
+        self.__data[self.__key(element)] = element
+
+    def discard(self, element: _Value) -> None:
+        del self.__data[self.__key(element)]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,4 @@
+from boiling_learning.utils.collections import KeyedSet
 from boiling_learning.utils.utils import indexify
 from unittest.case import TestCase
 
@@ -8,3 +9,22 @@ class utils_utils_test(TestCase):
             tuple(indexify('abc')),
             (0, 1, 2)
         )
+
+
+class utils_collections_test(TestCase):
+    def test_KeyedSet(self):
+        keyed_set = KeyedSet(str.upper, ('hi', 'bye', 'hello'))
+
+        self.assertEqual(set(keyed_set), {'hi', 'bye', 'hello'})
+        self.assertEqual(set(keyed_set.values()), {'hi', 'bye', 'hello'})
+        self.assertEqual(set(keyed_set.keys()), {'HI', 'BYE', 'HELLO'})
+        self.assertEqual(len(keyed_set), 3)
+        self.assertEqual(keyed_set['BYE'], 'bye')
+        self.assertIn('hello', keyed_set)
+        self.assertNotIn('byello', keyed_set)
+
+        keyed_set.add('byello')
+        self.assertIn('byello', keyed_set)
+
+        keyed_set.discard('hello')
+        self.assertNotIn('hello', keyed_set)
