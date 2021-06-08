2adbea6cf1c31af467bce913d14b6d7baad99fa5
Ruan Comelli <ruancomelli@gmail.com>
2021-06-08 16:34:29 -0300

    Define storage module for custom JSON encoders and decoders

---
+++ boiling_learning/io/storage.py
@@ -0,0 +1,36 @@
+from importlib import import_module
+import json
+from typing import Any, Dict, TypeVar
+
+from plum import Dispatcher
+
+from boiling_learning.utils.table_dispatch import table_dispatch
+
+_T = TypeVar('_T')
+dispatch = Dispatcher()
+
+
+@dispatch
+def json_encode(obj: Any) -> Any:
+    # default implementation: return object unmodified
+    return obj
+
+
+@table_dispatch
+def json_decode(obj: Any) -> Any:
+    # default implementation: return object unmodified
+    return obj
+
+
+def json_serialize(obj: Any) -> Dict[str, Any]:
+    return {
+        'module': type(obj).__module__,
+        'type': type(obj).__qualname__,
+        'contents': json_encode(obj)
+    }
+
+
+def json_deserialize(obj: Dict[str, Any]) -> Any:
+    module = import_module(obj['module'])
+    obj_type = getattr(module, obj['type'])
+    return json_decode(obj_type)(obj['contents'])
---
+++ tests/test_io.py
@@ -0,0 +1,44 @@
+import json
+
+from boiling_learning.io.storage import json_decode, json_deserialize, json_encode, json_serialize
+from unittest import TestCase
+
+
+class storage_Test(TestCase):
+    def test_default_json_io(self):
+        test_list = [
+            314159,
+            'apple pie tastes good',
+            {
+                'likes dinos': True,
+                'political opinion': None
+            }
+        ]
+
+        encoded = json.dumps(json_serialize(test_list))
+        decoded = json_deserialize(json.loads(encoded))
+
+        self.assertListEqual(decoded, test_list)
+
+    def test_custom_json_io(self):
+        test_tuple = (
+            314159,
+            'apple pie tastes good',
+            {
+                'likes dinos': True,
+                'political opinion': None
+            }
+        )
+
+        @json_encode.dispatch
+        def _json_encode(obj: tuple):
+            return list(obj)
+
+        @json_decode.dispatch(tuple)
+        def _json_decode(obj):
+            return tuple(obj)
+
+        encoded = json.dumps(json_serialize(test_tuple))
+        decoded = json_deserialize(json.loads(encoded))
+
+        self.assertTupleEqual(decoded, test_tuple)
