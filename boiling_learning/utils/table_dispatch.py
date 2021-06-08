ee3c4d121e37f7cea7d5382d6124332f0b5f0072
Ruan Comelli <ruancomelli@gmail.com>
2021-06-08 14:58:14 -0300

    Implement table dispatching

---
+++ boiling_learning/utils/table_dispatch.py
@@ -0,0 +1,32 @@
+from typing import Callable, Dict, Hashable, Optional, Tuple
+
+
+class DispatchError(Exception):
+    pass
+
+
+class TableDispatcher:
+    def __init__(self, call: Optional[Callable] = None):
+        self._default: Optional[Callable] = call
+        self._dispatch_table: Dict[Tuple[Hashable, ...], Callable] = {}
+
+    def dispatch(self, *keys: Hashable):
+        def _dispatcher(call: Callable) -> TableDispatcher:
+            self._dispatch_table[keys] = call
+            return self
+        return _dispatcher
+
+    def __call__(self, *keys: Hashable):
+        try:
+            return self._dispatch_table[keys]
+        except KeyError as e:
+            if self._dispatch is not None:
+                return self._dispatch
+            else:
+                raise DispatchError(
+                    'no default callable'
+                    f' and no dispatch form was provided for keys "{keys}".'
+                ) from e
+
+
+table_dispatch = TableDispatcher
