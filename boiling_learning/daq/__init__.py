f0a8dfcca3d82e44418dab6b1992e19761eba0b2
Ruan Comelli <ruancomelli@gmail.com>
2021-06-09 12:38:02 -0300

    Run DAQ tests only if NIDAQMx is not installed on system

--- boiling_learning/daq/__init__.py
+++ boiling_learning/daq/__init__.py
@@ -1,2 +1,2 @@
-from boiling_learning.daq.Channel import Channel, ChannelType
+from boiling_learning.daq.Channel import Channel, ChannelType, NIChannelType
 from boiling_learning.daq.Device import Device
