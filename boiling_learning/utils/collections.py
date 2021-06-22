59bc2c58a3391691abd7757c311ea6d6b13879a7
Ruan Comelli <ruancomelli@gmail.com>
2021-06-22 17:12:12 -0300

    feat(simplify-image-datasets): add more set-like methods to *KeyedSet*

--- boiling_learning/utils/collections.py
+++ boiling_learning/utils/collections.py
@@ -24,17 +24,11 @@ class KeyedSet(MutableSet[_Value], Generic[_Key, _Value]):
             self.__key(element): element for element in iterable
         }
 
-    def keys(self) -> KeysView:
-        return self.__data.keys()
-
-    def values(self) -> ValuesView:
-        return self.__data.values()
-
+    # ------------------------------------------------------------------------
+    # Set-like operations
+    # ------------------------------------------------------------------------
     def __len__(self) -> int:
         return len(self.__data)
-
-    def __getitem__(self, key: _Key) -> _Value:
-        return self.__data[key]
 
     def __contains__(self, element: _Value) -> bool:
         return element in self.values()
@@ -45,5 +39,29 @@ class KeyedSet(MutableSet[_Value], Generic[_Key, _Value]):
     def add(self, element: _Value) -> None:
         self.__data[self.__key(element)] = element
 
+    def remove(self, element: _Value) -> None:
+        del self[self.__key(element)]
+
     def discard(self, element: _Value) -> None:
-        del self.__data[self.__key(element)]
+        try:
+            self.remove(element)
+        except KeyError:
+            pass
+
+    # TODO: implement other operations as in the standard docs:
+    # <https://docs.python.org/3/library/stdtypes.html#set-types-set-frozenset>
+
+    # ------------------------------------------------------------------------
+    # Dict-like operations
+    # ------------------------------------------------------------------------
+    def __getitem__(self, key: _Key) -> _Value:
+        return self.__data[key]
+
+    def __delitem__(self, key: _Key) -> None:
+        del self.__data[key]
+
+    def keys(self) -> KeysView:
+        return self.__data.keys()
+
+    def values(self) -> ValuesView:
+        return self.__data.values()
