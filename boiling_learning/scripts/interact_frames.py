fix(visualization): fix image scaling when forwarding them to `cv2.imshow`
OpenCV expects image pixels to have intensity in the range [0, 255], but we are following Scikit-image and TensorFlow standard by making floating point image representation to remain inside [0, 1].

boiling_learning/scripts/interact_frames.py
@@ -1,6 +1,7 @@
 import warnings
 from typing import Any, Callable, Iterable, Tuple
 
+import funcy
 import numpy as np
 from ipywidgets import interact, widgets
 from skimage.transform import downscale_local_mean as downscale
@@ -14,7 +15,10 @@ def _make_show_frame_function(
     downscale_factor: Iterable[int] = (8, 8, 1),
 ) -> Callable[[np.ndarray], None]:
     def _imshow(image: np.ndarray) -> None:
-        imshow(downscale(image, downscale_factor))
+        # since we are just visualizing, there is no need to retain
+        # the full image resolution
+        image = downscale(image, downscale_factor)
+        imshow(image)
 
     return _imshow
 
@@ -79,16 +83,19 @@ def main(
     imshow_imported: bool = False
     if colab_backend:
         try:
-            from google.colab.patches import cv2_imshow
+            from google.colab.patches import cv2_imshow as imshow
 
             imshow_imported = True
         except (ImportError, ModuleNotFoundError):
             pass
 
     if not imshow_imported:
-        from cv2 import imshow as cv2_imshow
+        from cv2 import imshow
 
-    imshow = _make_show_frame_function(cv2_imshow)
+    # rescale images since float images are considered to be in [0, 1]
+    # but cv2_imshow expects them to be [0, 255]
+    imshow = funcy.compose(imshow, lambda img: img * 255)
+    imshow = _make_show_frame_function(imshow)
 
     with warnings.catch_warnings():
         _interact_dataset_frames(datasets, imshow)