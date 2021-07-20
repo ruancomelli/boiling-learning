d676e1efb0d8e541e8d0ebf5465149c27d864e7e
Ruan Comelli <ruancomelli@gmail.com>
2021-07-20 14:52:47 -0300

    refactor: pre-import common subpackages directly on the package's __init__ file
    Pre-importing subpackages makes it easier for autocomplete tools to find classes and functions inside them, speeding up a bit the development stages.

--- boiling_learning/__init__.py
+++ boiling_learning/__init__.py
@@ -1,6 +1,14 @@
 from importlib_metadata import version as _version
 from semver import VersionInfo
 
+# pre-import common subpackages
+import boiling_learning.datasets
+import boiling_learning.io
+import boiling_learning.management
+import boiling_learning.model
+import boiling_learning.preprocessing
+import boiling_learning.utils
+
 __all__ = ['__version__', 'version_info', 'version_compact']
 
 __version__ = _version('boiling_learning')
