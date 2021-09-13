ddc6935d94f4de5be17ed21b19a35cae52061846
Ruan Comelli <ruancomelli@gmail.com>
2021-09-13 09:36:32 -0300

    feat(management): add JSON cacher for easier caching of simple types

--- boiling_learning/management/cacher.py
+++ boiling_learning/management/cacher.py
@@ -4,9 +4,16 @@ from typing import Callable, Iterable, TypeVar, Union
 
 import wrapt
 
-from boiling_learning.io.io import LoaderFunction, SaverFunction
+from boiling_learning.io.io import (
+    LoaderFunction,
+    SaverFunction,
+    load_json,
+    save_json,
+)
+from boiling_learning.management.allocators import default_table_allocator
 from boiling_learning.management.persister import FileProvider, Provider
 from boiling_learning.utils.functional import Pack
+from boiling_learning.utils.utils import PathLike
 
 _T = TypeVar('_T')
 _Callable = TypeVar('_Callable', bound=Callable)
@@ -43,3 +50,14 @@ def cache(
         return provider.provide()
 
     return cacher
+
+
+def json_cache(
+    root: PathLike, autosave: bool = True
+) -> Callable[[_Callable], _Callable]:
+    return cache(
+        allocator=default_table_allocator(root),
+        saver=save_json,
+        loader=load_json,
+        autosave=autosave,
+    )
