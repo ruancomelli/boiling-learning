880c876b4f41d10bb700cc3bf9828f37d067f511
Ruan Comelli <ruancomelli@gmail.com>
2021-09-26 22:32:10 -0300

    feat(scripts): accept verbosity flag in `connect_gpus` script and add check for NVIDIA output

--- boiling_learning/scripts/connect_gpus.py
+++ boiling_learning/scripts/connect_gpus.py
@@ -2,9 +2,13 @@ from typing import Optional
 
 import tensorflow as tf
 
+from boiling_learning.utils import print_verbose
+
 
 def main(
-    require_gpu: bool = True, nvidia_output: Optional[str] = None
+    require_gpu: bool = True,
+    nvidia_output: Optional[str] = None,
+    verbose: bool = True,
 ) -> tf.distribute.Strategy:
     # See <https://www.tensorflow.org/xla/tutorials/autoclustering_xla>
     tf.config.optimizer.set_jit(True)  # Enable XLA.
@@ -12,25 +16,23 @@ def main(
     cpus = tf.config.list_physical_devices('CPU')
     gpus = tf.config.list_physical_devices('GPU')
 
-    print('Available CPUs:', cpus)
-    print('Available GPUs:', gpus)
+    print_verbose(verbose, 'Available CPUs:', cpus)
+    print_verbose(verbose, 'Available GPUs:', gpus)
 
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)
 
     if gpus:
-        print('Connected to GPUs.')
+        print_verbose(verbose, 'Connected to GPUs.')
         return tf.distribute.MirroredStrategy(
-            cross_device_ops=tf.distribute.NcclAllReduce(
-                num_packs=0
-            )  # trying to reduce memory usage
+            cross_device_ops=tf.distribute.NcclAllReduce(num_packs=0)
         )
-    elif nvidia_output.startswith('GPU'):
+    elif nvidia_output is not None and nvidia_output.startswith('GPU'):
         raise RuntimeError(
             'command *nvidia-smi -L* found GPUs, but TensorFlow could not connect to them.'
         )
     elif require_gpu:
         raise RuntimeError('No GPUs connected.')
     else:
-        print('No GPUs connected.')
+        print_verbose(verbose, 'No GPUs connected.')
         return tf.distribute.get_strategy()
