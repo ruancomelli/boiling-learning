b305f6aa70783b26250369b55300d244365a42c5
Ruan Comelli <ruancomelli@gmail.com>
2022-04-15 17:27:31 -0300

    refactor(scripts/set_boiling_cases_data): refactor module and split functions

--- boiling_learning/scripts/set_boiling_cases_data.py
+++ boiling_learning/scripts/set_boiling_cases_data.py
@@ -4,6 +4,7 @@ import numpy as np
 from loguru import logger
 
 from boiling_learning.preprocessing.cases import Case
+from boiling_learning.preprocessing.experiment_video import ExperimentVideo
 from boiling_learning.preprocessing.experimental_data import ExperimentalData
 from boiling_learning.utils import PathLike, geometry
 from boiling_learning.utils.frozendict import frozendict
@@ -33,66 +34,81 @@ def main(cases: Iterable[Case], *, case_experiment_map: Mapping[str, PathLike]) 
     logger.info('Setting boiling data')
 
     for case in cases:
-        case.set_video_data_from_file(purge=True, remove_absent=True)
+        set_case(case, case_experiment_path=case_experiment_map[case.name])
 
-        try:
-            logger.debug(f'Trying to load data for {case.name}')
-            case.load_dfs(overwrite=False, missing_ok=False)
-            logger.debug(f'Succesfully loaded data for {case.name}')
-        except FileNotFoundError as e:
-            logger.debug(f'Failed to load data, making dataframes for {case.name}')
 
-            df = ExperimentalData(data_path=case_experiment_map[case.name]).as_dataframe()
+def set_case(case: Case, *, case_experiment_path: PathLike) -> None:
+    case.set_video_data_from_file(purge=True, remove_absent=True)
 
-            df = df.drop(columns='Time instant').astype({'Elapsed time': 'float64'})
-            df = df.set_index('Elapsed time')
+    try:
+        logger.debug(f'Trying to load data for {case.name}')
+        case.load_dfs(overwrite=False, missing_ok=False)
+        logger.debug(f'Succesfully loaded data for {case.name}')
+    except FileNotFoundError:
+        logger.debug(f'Failed to load data, making dataframes for {case.name}')
 
-            case.sync_time_series(df)
-            case.make_dataframe(
-                recalculate=False,
-                exist_load=False,
-                enforce_time=True,
-                categories_as_int=True,
-                inplace=True,
-            )
+        df = (
+            ExperimentalData(data_path=case_experiment_path)
+            .as_dataframe()
+            .drop(columns='Time instant')
+            .astype({'Elapsed time': 'float64'})
+            .set_index('Elapsed time')
+        )
 
-            for ev in case.values():
-                _indices = tuple(map(int, ev.df[ev.column_names.index]))
-                _expected = tuple(range(len(ev.video)))
-                if _indices != _expected:
-                    raise ValueError(
-                        f'expected indices != indices for {ev.name}.'
-                        f' Got expected: {_expected}'
-                        f' Got indices: {_indices}'
-                    ) from e
+        case.sync_time_series(df)
+        case.make_dataframe(
+            recalculate=False,
+            exist_load=False,
+            enforce_time=True,
+            categories_as_int=True,
+            inplace=True,
+        )
 
-            for ev in case.values():
-                ev.df = ev.df.drop(
-                    columns=[
-                        'Time instant',
-                        'Flux [W/m^2]',
-                        'Flux [W/cm^2]',
-                        'Bulk Temperature [deg C]',
-                        'Wire Temperature [deg C]',
-                        'Superheat [deg C]',
-                    ],
-                    errors='ignore',
-                )
+        for ev in case:
+            _check_experiment_video(ev)
 
-                power_unit = ureg.watt
-                heat_flux_unit = ureg.watt / ureg.centimeter**2
-                sample_id = ev.data.categories['sample_id']
+        for ev in case:
+            _regularize_experiment_video_dataframe(ev)
 
-                full_power_key = add_unit_post_fix('Power', power_unit)
-                full_heat_flux_key = add_unit_post_fix('Flux', heat_flux_unit)
+        case.save_dfs(overwrite=False)
 
-                lateral_area = SAMPLES[sample_id].lateral_area
-                power = np.array(ev.df[full_power_key]) * power_unit
-                flux = power / lateral_area
 
-                ev.df[full_heat_flux_key] = flux.to(heat_flux_unit).magnitude
+def _regularize_experiment_video_dataframe(ev: ExperimentVideo) -> None:
+    ev.df = ev.df.drop(
+        columns=[
+            'Time instant',
+            'Flux [W/m^2]',
+            'Flux [W/cm^2]',
+            'Bulk Temperature [deg C]',
+            'Wire Temperature [deg C]',
+            'Superheat [deg C]',
+        ],
+        errors='ignore',
+    )
 
-            case.save_dfs(overwrite=False)
+    power_unit = ureg.watt
+    heat_flux_unit = ureg.watt / ureg.centimeter**2
+    sample_id = ev.data.categories['sample_id']
+
+    full_power_key = add_unit_post_fix('Power', power_unit)
+    full_heat_flux_key = add_unit_post_fix('Flux', heat_flux_unit)
+
+    lateral_area = SAMPLES[sample_id].lateral_area
+    power = np.array(ev.df[full_power_key]) * power_unit
+    flux = power / lateral_area
+
+    ev.df[full_heat_flux_key] = flux.to(heat_flux_unit).magnitude
+
+
+def _check_experiment_video(ev: ExperimentVideo) -> None:
+    indices = tuple(map(int, ev.df[ev.column_names.index]))
+    expected = tuple(range(len(ev.video)))
+    if indices != expected:
+        raise ValueError(
+            f'expected indices != indices for {ev.name}.'
+            f' Got expected: {expected}.'
+            f' Got indices: {indices}.'
+        )
 
 
 if __name__ == '__main__':
