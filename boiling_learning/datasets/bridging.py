bd17d2c978c742261ddfcb1d656e25e67c610cb2
Ruan Comelli <ruancomelli@gmail.com>
2022-06-04 19:12:40 -0300

    feat(datasets/bridging): add support for filtering in `sliceable_dataset_to_tensorflow_dataset`

--- boiling_learning/datasets/bridging.py
+++ boiling_learning/datasets/bridging.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Optional, Union
+from typing import Any, Callable, Optional, Union
 
 import tensorflow as tf
 
@@ -16,11 +16,15 @@ def sliceable_dataset_to_tensorflow_dataset(
     prefetch: bool = False,
     snapshot_path: Optional[PathLike] = None,
     cache: Union[bool, PathLike] = False,
+    filter_predicate: Optional[Callable[[Any], bool]] = None,
 ) -> tf.data.Dataset:
     sample = dataset[0]
     typespec = auto_spec(sample)
 
     ds = tf.data.Dataset.from_generator(lambda: dataset, output_signature=typespec)
+
+    if filter_predicate is not None:
+        ds = ds.filter(filter_predicate)
 
     if snapshot_path is not None:
         ds = ds.snapshot(str(resolve(snapshot_path, parents=True)))
