f7afabe1fbf0387c86c1a705ec6aeaa44a7ae966
Ruan Comelli <ruancomelli@gmail.com>
2022-06-04 19:21:16 -0300

    feat(datasets/bridging): add support for shuffling and auto-fixing batch size issues to `sliceable_dataset_to_tensorflow_dataset`

--- boiling_learning/datasets/bridging.py
+++ boiling_learning/datasets/bridging.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import Any, Callable, Optional, Union
+import random
+from typing import Any, Callable, Optional, TypeVar, Union
 
 import tensorflow as tf
 
@@ -8,16 +9,29 @@ from boiling_learning.datasets.sliceable import SliceableDataset
 from boiling_learning.utils import PathLike, resolve
 from boiling_learning.utils.dtypes import auto_spec
 
+_T = TypeVar('_T')
+
 
 def sliceable_dataset_to_tensorflow_dataset(
     dataset: SliceableDataset[Any],
     *,
     batch_size: Optional[int] = None,
+    filter_predicate: Optional[Callable[[Any], bool]] = None,
     prefetch: bool = False,
+    shuffle: bool = False,
+    expand_to_batch_size: bool = False,
     snapshot_path: Optional[PathLike] = None,
     cache: Union[bool, PathLike] = False,
-    filter_predicate: Optional[Callable[[Any], bool]] = None,
 ) -> tf.data.Dataset:
+    if shuffle:
+        dataset = dataset.shuffle()
+
+    if expand_to_batch_size and batch_size is not None:
+        dataset = _expand_to_batch_size(dataset, batch_size=batch_size)
+
+    if prefetch:
+        dataset = dataset.prefetch(batch_size)
+
     sample = dataset[0]
     typespec = auto_spec(sample)
 
@@ -30,7 +44,12 @@ def sliceable_dataset_to_tensorflow_dataset(
         ds = ds.snapshot(str(resolve(snapshot_path, parents=True)))
 
     if batch_size is not None:
-        ds = ds.batch(batch_size)
+        ds = ds.batch(
+            batch_size,
+            drop_remainder=expand_to_batch_size,
+            num_parallel_calls=tf.data.AUTOTUNE,
+            deterministic=False,
+        )
 
     if cache:
         ds = ds.cache() if isinstance(cache, bool) else ds.cache(str(resolve(cache, parents=True)))
@@ -39,3 +58,13 @@ def sliceable_dataset_to_tensorflow_dataset(
         ds = ds.prefetch(tf.data.AUTOTUNE)
 
     return ds
+
+
+def _expand_to_batch_size(
+    dataset: SliceableDataset[_T], *, batch_size: int
+) -> SliceableDataset[_T]:
+    dataset_size = len(dataset)
+    missing_count = batch_size - dataset_size % batch_size
+    all_indices = range(dataset_size)
+    some_indices = random.sample(all_indices, missing_count)
+    return dataset.concatenate(dataset[some_indices])
