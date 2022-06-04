39b44a740a47caf585bafd903db55e7538a59d38
Ruan Comelli <ruancomelli@gmail.com>
2022-06-04 19:24:36 -0300

    refactor(model/training): remove unnecessary arguments to `model.fit` and use new functionality from the bridging module

--- boiling_learning/model/training.py
+++ boiling_learning/model/training.py
@@ -110,28 +110,26 @@ def get_fit_model(
     ds_train, ds_val, _ = datasets.value
 
     ds_train = sliceable_dataset_to_tensorflow_dataset(
-        ds_train.shuffle().prefetch(params.batch_size),
+        ds_train,
         batch_size=params.batch_size,
         prefetch=True,
+        shuffle=True,
+        expand_to_batch_size=True,
         cache=(cache if isinstance(cache, bool) else cache / 'train'),
         snapshot_path=(snapshot_path / 'train' if snapshot_path is not None else None),
     )
     ds_val = sliceable_dataset_to_tensorflow_dataset(
-        ds_val.shuffle().prefetch(params.batch_size),
+        ds_val,
         batch_size=params.batch_size,
         prefetch=True,
+        shuffle=True,
+        expand_to_batch_size=True,
         cache=(cache if isinstance(cache, bool) else cache / 'val'),
         snapshot_path=(snapshot_path / 'val' if snapshot_path is not None else None),
     )
 
     model.fit(
-        ds_train,
-        batch_size=params.batch_size,
-        epochs=params.epochs,
-        validation_data=ds_val,
-        callbacks=params.callbacks.value,
-        use_multiprocessing=True,
-        workers=-1,
+        ds_train, validation_data=ds_val, epochs=params.epochs, callbacks=params.callbacks.value
     )
 
     return model
