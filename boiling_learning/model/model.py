839a61a37ac9ebe640d0a3396397c3b9e3753a60
Ruan Comelli <ruancomelli@gmail.com>
2022-06-11 11:39:14 -0300

    feat(model/model): add utility methods for counting the number of parameters and the size of a model

--- boiling_learning/model/model.py
+++ boiling_learning/model/model.py
@@ -7,9 +7,11 @@ from pathlib import Path
 from typing import Any, Dict
 
 import tensorflow as tf
+from pint import Quantity
 
 from boiling_learning.io import json
 from boiling_learning.io.storage import Metadata, deserialize, serialize
+from boiling_learning.utils.units import unit_registry as ureg
 from boiling_learning.utils.utils import resolve
 
 
@@ -55,6 +57,66 @@ def _deserialize_model(path: Path, _metadata: Metadata) -> ModelArchitecture:
     architecture = ModelArchitecture(tf.keras.models.model_from_json(model_json_path.read_text()))
     architecture.model.load_weights(str(model_weights_path))
     return architecture
+
+
+def model_memory_usage_in_bytes(
+    architecture: ModelArchitecture, *, batch_size: int
+) -> Quantity[int]:
+    """
+    Return the estimated memory usage of a given Keras model in bytes.
+    This includes the model weights and layers, but excludes the dataset.
+
+    The model shapes are multipled by the batch size, but the weights are not.
+
+    Source: https://stackoverflow.com/a/64359137/5811400
+
+    Args:
+        model: A Keras model.
+        batch_size: The batch size you intend to run the model with. If you
+            have already specified the batch size in the model itself, then
+            pass `1` as the argument here.
+    Returns:
+        An estimate of the Keras model's memory usage in bytes.
+
+    """
+    model = architecture.model
+
+    default_dtype = tf.keras.backend.floatx()
+    shapes_mem_count = 0
+    internal_model_mem_count = 0 * ureg.byte
+    for layer in model.layers:
+        if isinstance(layer, tf.keras.Model):
+            internal_model_mem_count += model_memory_usage_in_bytes(layer, batch_size=batch_size)
+
+        single_layer_mem = tf.as_dtype(layer.dtype or default_dtype).size
+        out_shape = layer.output_shape
+        if isinstance(out_shape, list):
+            out_shape = out_shape[0]
+        for s in out_shape:
+            if s is None:
+                continue
+            single_layer_mem *= s
+        shapes_mem_count += single_layer_mem
+
+    model_params_count = count_model_parameters(architecture)
+    return (
+        batch_size * shapes_mem_count + model_params_count
+    ) * ureg.byte + internal_model_mem_count
+
+
+def count_model_parameters(
+    architecture: ModelArchitecture, *, trainable: bool = True, non_trainable: bool = True
+) -> int:
+    model = architecture.model
+
+    if trainable and non_trainable:
+        return typing.cast(int, model.count_params())
+    elif trainable:
+        return sum(tf.keras.backend.count_params(p) for p in model.trainable_weights)
+    elif non_trainable:
+        return sum(tf.keras.backend.count_params(p) for p in model.non_trainable_weights)
+    else:
+        raise ValueError('at least one of `trainable` and `non_trainable` must be true')
 
 
 class ProblemType(enum.Enum):
