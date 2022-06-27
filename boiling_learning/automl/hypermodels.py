7f323e379e89bc501ad1365d54176d3710a0d931
Ruan Comelli <ruancomelli@gmail.com>
2022-06-26 22:27:54 -0300

    feat(automl/hypermodels): define `FixedArchitectureImageRegressor` automodel

--- boiling_learning/automl/hypermodels.py
+++ boiling_learning/automl/hypermodels.py
@@ -1,9 +1,10 @@
 import typing
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 import autokeras as ak
 import tensorflow as tf
 
+from boiling_learning.automl.blocks import LayersBlock
 from boiling_learning.io import json
 from boiling_learning.model.model import anonymize_model_json
 
@@ -30,14 +31,29 @@ class ImageRegressor(ak.AutoModel):
         self,
         loss: tf.keras.losses.Loss,
         metrics: List[tf.keras.metrics.Metric],
-        normalize_images: bool,
-        augment_images: bool,
+        normalize_images: Optional[bool] = None,
+        augment_images: Optional[bool] = None,
         **kwargs: Any,
     ) -> None:
         inputs = ak.ImageInput()
         outputs = ak.ImageBlock(normalize=normalize_images, augment=augment_images)(inputs)
         outputs = ak.SpatialReduction()(outputs)
         outputs = ak.DenseBlock()(outputs)
+        outputs = ak.RegressionHead(output_dim=1, loss=loss, metrics=metrics)(outputs)
+
+        super().__init__(inputs, outputs, **kwargs)
+
+
+class FixedArchitectureImageRegressor(ak.AutoModel):
+    def __init__(
+        self,
+        layers: List[tf.keras.layers.Layer],
+        loss: tf.keras.losses.Loss,
+        metrics: List[tf.keras.metrics.Metric],
+        **kwargs: Any,
+    ) -> None:
+        inputs = ak.ImageInput()
+        outputs = LayersBlock(layers)(inputs)
         outputs = ak.RegressionHead(output_dim=1, loss=loss, metrics=metrics)(outputs)
 
         super().__init__(inputs, outputs, **kwargs)
