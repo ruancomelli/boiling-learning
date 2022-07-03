838739daad2b352361d062476aa13b8ffae8fc39
Ruan Comelli <ruancomelli@gmail.com>
2022-07-02 21:24:30 -0300

    feat(automl/hypermodels): accept allocators for saving hypermodels

--- boiling_learning/automl/hypermodels.py
+++ boiling_learning/automl/hypermodels.py
@@ -1,5 +1,5 @@
 import typing
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 
 import autokeras as ak
 import keras_tuner as kt
@@ -7,7 +7,10 @@ import tensorflow as tf
 
 from boiling_learning.automl.blocks import LayersBlock
 from boiling_learning.io import json
+from boiling_learning.management.allocators import Allocator
 from boiling_learning.model.model import anonymize_model_json
+from boiling_learning.utils.described import Described
+from boiling_learning.utils.utils import PathLike, resolve
 
 
 class HyperModel(kt.HyperModel):
@@ -33,15 +36,44 @@ class ImageRegressor(HyperModel):
         metrics: List[tf.keras.metrics.Metric],
         normalize_images: Optional[bool] = None,
         augment_images: Optional[bool] = None,
+        directory: Union[PathLike, Allocator, None] = None,
+        strategy: Optional[Described[tf.distribute.Strategy, Any]] = None,
         **kwargs: Any,
     ) -> None:
+        if 'overwrite' in kwargs:
+            raise TypeError("the argument 'overwrite' is not supported.")
+
+        if 'distribution_strategy' in kwargs:
+            raise TypeError("the argument 'distribution_strategy' is not supported.")
+
         inputs = ak.ImageInput()
         outputs = ak.ImageBlock(normalize=normalize_images, augment=augment_images)(inputs)
         outputs = ak.SpatialReduction()(outputs)
         outputs = ak.DenseBlock()(outputs)
         outputs = ak.RegressionHead(output_dim=1, loss=loss, metrics=metrics)(outputs)
 
-        super().__init__(ak.AutoModel(inputs, outputs, **kwargs))
+        if isinstance(directory, Allocator):
+            directory = directory.allocate(
+                self.__class__.__name__,
+                loss=loss,
+                metrics=metrics,
+                normalize_images=normalize_images,
+                augment_images=augment_images,
+                **kwargs,
+            )
+        elif directory is not None:
+            directory = resolve(directory, parents=True)
+
+        super().__init__(
+            ak.AutoModel(
+                inputs,
+                outputs,
+                directory=directory,
+                overwrite=directory is None,
+                distribution_strategy=strategy.value if strategy is not None else None,
+                **kwargs,
+            )
+        )
 
 
 class FixedArchitectureImageRegressor(HyperModel):
@@ -50,10 +82,38 @@ class FixedArchitectureImageRegressor(HyperModel):
         layers: List[tf.keras.layers.Layer],
         loss: tf.keras.losses.Loss,
         metrics: List[tf.keras.metrics.Metric],
+        directory: Union[PathLike, Allocator, None] = None,
+        strategy: Optional[Described[tf.distribute.Strategy, Any]] = None,
         **kwargs: Any,
     ) -> None:
+        if 'overwrite' in kwargs:
+            raise TypeError("the argument 'overwrite' is not supported.")
+
+        if 'distribution_strategy' in kwargs:
+            raise TypeError("the argument 'distribution_strategy' is not supported.")
+
         inputs = ak.ImageInput()
         outputs = LayersBlock(layers)(inputs)
         outputs = ak.RegressionHead(output_dim=1, loss=loss, metrics=metrics)(outputs)
 
-        super().__init__(ak.AutoModel(inputs, outputs, **kwargs))
+        if isinstance(directory, Allocator):
+            directory = directory.allocate(
+                self.__class__.__name__,
+                loss=loss,
+                metrics=metrics,
+                layers=[layer.get_config() for layer in layers],
+                **kwargs,
+            )
+        elif directory is not None:
+            directory = resolve(directory, parents=True)
+
+        super().__init__(
+            ak.AutoModel(
+                inputs,
+                outputs,
+                directory=directory,
+                overwrite=directory is None,
+                distribution_strategy=strategy.value if strategy is not None else None,
+                **kwargs,
+            )
+        )
