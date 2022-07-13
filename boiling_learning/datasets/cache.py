3a65103eef60d5872f86754413336b0fb292d8a8
Ruan Comelli <ruancomelli@gmail.com>
2022-07-12 22:59:38 -0300

    feat(datasets/cache): support constructing a numpy cache adjusted to a dataset

--- boiling_learning/datasets/cache.py
+++ boiling_learning/datasets/cache.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import json as _json
 from pathlib import Path
 from typing import Dict, Iterable, Optional, Tuple, TypeVar
@@ -6,20 +8,39 @@ import numpy as np
 from loguru import logger
 from typing_extensions import Literal
 
-from boiling_learning.datasets.sliceable import SliceableDatasetCache
+from boiling_learning.datasets.sliceable import SliceableDataset, SliceableDatasetCache
 from boiling_learning.utils.iterutils import unsort
 from boiling_learning.utils.utils import PathLike, resolve
 
-_T = TypeVar('_T')
+_Array = TypeVar('_Array', bound=np.ndarray)
 
 
-class NumpyCache(SliceableDatasetCache[_T]):
-    def __init__(self, directory: PathLike, shape: Tuple[int, ...], dtype: np.dtype) -> None:
+class NumpyCache(SliceableDatasetCache[_Array]):
+    def __init__(
+        self,
+        directory: PathLike,
+        *,
+        shape: Tuple[int, ...],
+        dtype: np.dtype,
+    ) -> None:
         self._directory = resolve(directory, dir=True)
         self._shape = shape
         self._dtype = dtype
 
-    def store(self, pairs: Dict[int, np.ndarray]) -> None:
+    @classmethod
+    def from_dataset(
+        cls,
+        directory: PathLike,
+        dataset: SliceableDataset[_Array],
+    ) -> NumpyCache[_Array]:
+        first_frame = dataset[0]
+        return NumpyCache(
+            directory,
+            shape=(len(dataset), *first_frame.shape),
+            dtype=first_frame.dtype,
+        )
+
+    def store(self, pairs: Dict[int, _Array]) -> None:
         logger.debug(f'Storing {len(pairs)} items {sorted(pairs)} to {self._data_path}')
 
         indices = list(pairs)
@@ -34,7 +55,7 @@ class NumpyCache(SliceableDatasetCache[_T]):
         with self._indices_path.open('w') as file:
             _json.dump(new_indices, file)
 
-    def fetch(self, indices: Optional[Iterable[int]] = None):
+    def fetch(self, indices: Optional[Iterable[int]] = None) -> Tuple[_Array, ...]:
         if indices is None:
             indices = sorted(self._current_indices())
 
