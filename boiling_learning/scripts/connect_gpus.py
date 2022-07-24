95154a664e8d03195f5421b8511a4a0b1aabf927
Ruan Comelli <ruancomelli@gmail.com>
2022-07-24 20:20:33 -0300

    refactor(scripts/connect_gpus): remove unnecessary `else` clauses

--- boiling_learning/scripts/connect_gpus.py
+++ boiling_learning/scripts/connect_gpus.py
@@ -13,24 +13,27 @@ def main(
     # See <https://www.tensorflow.org/xla/tutorials/autoclustering_xla>
     tf.config.optimizer.set_jit(True)  # Enable XLA.
 
-    gpus: List[str] = tf.config.list_physical_devices('GPU')
+    gpus: List[tf.config.PhysicalDevice] = tf.config.list_physical_devices('GPU')
 
     logger.info(f'Available GPUs: {gpus}')
 
-    for gpu in gpus:
-        tf.config.experimental.set_memory_growth(gpu, True)
-
     if gpus:
         logger.info('Connected to GPUs.')
+
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+
         return tf.distribute.MirroredStrategy(
             cross_device_ops=tf.distribute.NcclAllReduce(num_packs=0)
         )
-    elif nvidia_output is not None and nvidia_output.startswith('GPU'):
+
+    if nvidia_output is not None and nvidia_output.startswith('GPU'):
         raise RuntimeError(
             'command *nvidia-smi -L* found GPUs, but TensorFlow could not connect to them.'
         )
-    elif require_gpu:
+
+    if require_gpu:
         raise RuntimeError('No GPUs connected.')
-    else:
-        logger.info('No GPUs connected.')
-        return tf.distribute.get_strategy()
+
+    logger.info('No GPUs connected.')
+    return tf.distribute.get_strategy()
