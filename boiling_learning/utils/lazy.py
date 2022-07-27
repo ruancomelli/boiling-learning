34e710a62018bf4a008ba0ed7ae225a47d61ca71
Ruan Comelli <ruancomelli@gmail.com>
2022-07-26 22:23:48 -0300

    refactor(utils/lazy): remove unused `LazyCallable.__matmul__` operator overload

--- boiling_learning/utils/lazy.py
+++ boiling_learning/utils/lazy.py
@@ -3,12 +3,9 @@ from __future__ import annotations
 from functools import lru_cache, partial
 from typing import Any, Callable, Generic, TypeVar
 
-import funcy
-
 __all__ = ('Lazy', 'LazyCallable')
 
 _T = TypeVar('_T')
-_S = TypeVar('_S')
 
 
 class Lazy(Generic[_T]):
@@ -30,8 +27,3 @@ class LazyCallable(Generic[_T]):
 
     def __call__(self, *args: Any, **kwargs: Any) -> Lazy[_T]:
         return Lazy(partial(self._call, *args, **kwargs))
-
-    def __rmatmul__(self, other: Callable[[_T], _S]) -> LazyCallable[_S]:
-        if callable(other):
-            return LazyCallable(funcy.compose(other, self._call))
-        return NotImplemented
