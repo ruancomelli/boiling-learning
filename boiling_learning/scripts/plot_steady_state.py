84a902826e3af969870702aa6eb5df884e4908fc
Ruan Comelli <ruancomelli@gmail.com>
2022-07-30 12:21:40 -0300

    refactor(scripts): move steady state plotting script to `scripts` folder

---
+++ boiling_learning/scripts/plot_steady_state.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+from typing import Optional, Tuple
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import modin.pandas as pd
+import seaborn as sns
+
+STEADY_STATE_DIR = Path(__file__).parent.parent / 'Selected Experiments'
+STEADY_STATE_PATH = STEADY_STATE_DIR / 'SteadyState.csv'
+
+# ----------------------------------------------------------
+# Configuration
+# ----------------------------------------------------------
+
+
+def figsize(
+    height: Optional[float] = None,
+    width: Optional[float] = None,
+    ratio: Optional[float] = None,
+    textwidth: float = 6.29707,
+) -> Tuple[float, float]:
+    if height is None:
+        if width is None or ratio is None:
+            raise ValueError('if `height` is omitted, both `width` and `ratio` must be given.')
+        height = width * ratio
+
+    if width is None:
+        if height is None or ratio is None:
+            raise ValueError('if `width` is omitted, both `height` and `ratio` must be given.')
+        width = height / ratio
+
+    return (width * textwidth, height * textwidth)
+
+
+sns.set()
+sns.set_style('white')
+plt.rc('font', **{'family': 'serif', 'serif': ['Fourier']})
+# plt.rc('font',**{'family':'serif','serif':['Palatino']})
+plt.rc('text', usetex=True)
+
+mpl.use('pgf')
+mpl.rcParams.update(
+    {  # setup matplotlib to use latex for output
+        'pgf.texsystem': 'pdflatex',  # change this if using xetex or lautex
+        'text.usetex': True,  # use LaTeX to write all text
+        'font.family': 'serif',
+        'font.serif': [],  # blank entries should cause plots
+        'font.sans-serif': [],  # to inherit fonts from the document
+        'font.monospace': [],
+        'axes.labelsize': 12,  # LaTeX default is 10pt font.
+        'font.size': 12,
+        'legend.fontsize': 10,  # Make the legend/label fonts
+        'xtick.labelsize': 10,  # a little smaller
+        'ytick.labelsize': 10,
+        'figure.figsize': figsize(width=0.3, ratio=3 / 4),  # default fig size of 0.9 textwidth
+        'pgf.preamble': [
+            r'\usepackage[utf8x]{inputenc}',  # use utf8 fonts
+            r'\usepackage[T1]{fontenc}',  # plots will be generated
+            r'\usepackage{fourier}',  # plots will be generated
+            r'\usepackage[detect-all]{siunitx}',
+        ],  # using this preamble
+    }
+)
+
+# ----------------------------------------------------------
+# Configuration
+# ----------------------------------------------------------
+data = pd.read_csv(STEADY_STATE_PATH)
+
+elapsed_time_min = data['Elapsed time'] / 60
+
+plt.plot(elapsed_time_min, data['Temperature'])
+plt.xlabel('Elapsed Time [$\si{\min}$]')
+# plt.show()
+plt.savefig(STEADY_STATE_DIR / 'file.pdf')
