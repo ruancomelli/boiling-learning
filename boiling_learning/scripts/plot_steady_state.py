5b3991053361b998443fa2c7c49e7554746f2b26
Ruan Comelli <ruancomelli@gmail.com>
2022-08-02 21:59:10 -0300

    fix(scripts/plot_steady_state): wrap script in a function

--- boiling_learning/scripts/plot_steady_state.py
+++ boiling_learning/scripts/plot_steady_state.py
@@ -6,15 +6,54 @@ import matplotlib.pyplot as plt
 import modin.pandas as pd
 import seaborn as sns
 
-STEADY_STATE_DIR = Path(__file__).parent.parent / 'Selected Experiments'
-STEADY_STATE_PATH = STEADY_STATE_DIR / 'SteadyState.csv'
-
-# ----------------------------------------------------------
-# Configuration
-# ----------------------------------------------------------
+_STEADY_STATE_DIR = Path(__file__).parent.parent / 'Selected Experiments'
 
 
-def figsize(
+def main(
+    data_path: Path = _STEADY_STATE_DIR / 'SteadyState.csv',
+    output_path: Path = _STEADY_STATE_DIR / 'file.pdf',
+) -> None:
+    sns.set()
+    sns.set_style('white')
+    plt.rc('font', **{'family': 'serif', 'serif': ['Fourier']})
+    # plt.rc('font',**{'family':'serif','serif':['Palatino']})
+    plt.rc('text', usetex=True)
+
+    mpl.use('pgf')
+    mpl.rcParams.update(
+        {  # setup matplotlib to use latex for output
+            'pgf.texsystem': 'pdflatex',  # change this if using xetex or lautex
+            'text.usetex': True,  # use LaTeX to write all text
+            'font.family': 'serif',
+            'font.serif': [],  # blank entries should cause plots
+            'font.sans-serif': [],  # to inherit fonts from the document
+            'font.monospace': [],
+            'axes.labelsize': 12,  # LaTeX default is 10pt font.
+            'font.size': 12,
+            'legend.fontsize': 10,  # Make the legend/label fonts
+            'xtick.labelsize': 10,  # a little smaller
+            'ytick.labelsize': 10,
+            'figure.figsize': _figsize(width=0.3, ratio=0.75),  # default fig size of 0.9 textwidth
+            'pgf.preamble': [
+                r'\usepackage[utf8x]{inputenc}',  # use utf8 fonts
+                r'\usepackage[T1]{fontenc}',  # plots will be generated
+                r'\usepackage{fourier}',  # plots will be generated
+                r'\usepackage[detect-all]{siunitx}',
+            ],  # using this preamble
+        }
+    )
+
+    data = pd.read_csv(data_path)
+
+    elapsed_time_min = data['Elapsed time'] / 60
+
+    plt.plot(elapsed_time_min, data['Temperature'])
+    plt.xlabel('Elapsed Time [$\si{\min}$]')
+    # plt.show()
+    plt.savefig(output_path)
+
+
+def _figsize(
     height: Optional[float] = None,
     width: Optional[float] = None,
     ratio: Optional[float] = None,
@@ -31,46 +70,3 @@ def figsize(
         width = height / ratio
 
     return (width * textwidth, height * textwidth)
-
-
-sns.set()
-sns.set_style('white')
-plt.rc('font', **{'family': 'serif', 'serif': ['Fourier']})
-# plt.rc('font',**{'family':'serif','serif':['Palatino']})
-plt.rc('text', usetex=True)
-
-mpl.use('pgf')
-mpl.rcParams.update(
-    {  # setup matplotlib to use latex for output
-        'pgf.texsystem': 'pdflatex',  # change this if using xetex or lautex
-        'text.usetex': True,  # use LaTeX to write all text
-        'font.family': 'serif',
-        'font.serif': [],  # blank entries should cause plots
-        'font.sans-serif': [],  # to inherit fonts from the document
-        'font.monospace': [],
-        'axes.labelsize': 12,  # LaTeX default is 10pt font.
-        'font.size': 12,
-        'legend.fontsize': 10,  # Make the legend/label fonts
-        'xtick.labelsize': 10,  # a little smaller
-        'ytick.labelsize': 10,
-        'figure.figsize': figsize(width=0.3, ratio=3 / 4),  # default fig size of 0.9 textwidth
-        'pgf.preamble': [
-            r'\usepackage[utf8x]{inputenc}',  # use utf8 fonts
-            r'\usepackage[T1]{fontenc}',  # plots will be generated
-            r'\usepackage{fourier}',  # plots will be generated
-            r'\usepackage[detect-all]{siunitx}',
-        ],  # using this preamble
-    }
-)
-
-# ----------------------------------------------------------
-# Configuration
-# ----------------------------------------------------------
-data = pd.read_csv(STEADY_STATE_PATH)
-
-elapsed_time_min = data['Elapsed time'] / 60
-
-plt.plot(elapsed_time_min, data['Temperature'])
-plt.xlabel('Elapsed Time [$\si{\min}$]')
-# plt.show()
-plt.savefig(STEADY_STATE_DIR / 'file.pdf')
