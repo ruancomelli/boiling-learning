445ae226e90100f2addbccf0dbd1ea8898dbd0f8
Ruan Comelli <ruancomelli@gmail.com>
2022-09-19 22:56:47 -0300

    fix(scripts/make_boiling_processors): fix indirect visualization cropping by cropping from the bottom

--- boiling_learning/scripts/make_boiling_processors.py
+++ boiling_learning/scripts/make_boiling_processors.py
@@ -176,7 +176,7 @@ def main(
             'GOPR2959': Cropper(left=980, right=1810, top=400, bottom=1200),
             'GOPR2960': Cropper(left=980, right=1810, top=400, bottom=1200),
         },
-        Cropper(height=height * downscale_factor, bottom_border=0),
+        Cropper(height=height * downscale_factor, top=0),
         {'center': CenterCropper, 'random': RandomCropper}[crop_mode](
             width=width * downscale_factor
         ),
