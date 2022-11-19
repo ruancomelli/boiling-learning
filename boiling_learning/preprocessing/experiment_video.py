e8aada241a4cb9f6f3656251a736d89c4f9757a2
Ruan Comelli <ruancomelli@gmail.com>
2022-11-19 15:59:41 -0300

    refactor(preprocessing/experiment_video): remove unused parameter from `ExperimentVideo.targets`

--- boiling_learning/preprocessing/experiment_video.py
+++ boiling_learning/preprocessing/experiment_video.py
@@ -1,7 +1,7 @@
 import contextlib
 from datetime import timedelta
 from pathlib import Path
-from typing import Any, Iterator, Mapping, Optional, Union
+from typing import Any, Iterator, Mapping, Optional
 
 import funcy
 import modin.pandas as pd
@@ -301,13 +301,10 @@ class ExperimentVideo:
         if overwrite or not path.is_file():
             self.df.to_csv(path, index=False)
 
-    def targets(self, select_columns: Optional[Union[str, list[str]]] = None) -> pd.DataFrame:
+    def targets(self) -> pd.DataFrame:
         df = self.make_dataframe()
         df = self.convert_dataframe_type(df)
         df.sort_values(by=self.column_names.index, inplace=True)
-
-        if select_columns is not None:
-            df = df[select_columns]
 
         return df
 
