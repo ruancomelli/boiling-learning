fb0d08ca6b2c16ad6323d626ad907ade91e4d7d8
Ruan Comelli <ruancomelli@gmail.com>
2022-11-22 22:53:13 -0300

    chore(app): temporarily comment out unfinished functionality

--- boiling_learning/__main__.py
+++ boiling_learning/__main__.py
@@ -1,10 +1,10 @@
 import typer
 
-from boiling_learning.app.studies.validation import validate
+# from boiling_learning.app.studies.validation import validate
 
 app = typer.Typer()
 
-app.command()(validate)
+# app.command()(validate)
 
 
 if __name__ == '__main__':
--- boiling_learning/app/training/boiling.py
+++ boiling_learning/app/training/boiling.py
@@ -1,18 +1,18 @@
-from boiling_learning.model.training import FitModelReturn
+# from boiling_learning.model.training import FitModelReturn
 
 
-def get_pretrained_baseline_boiling_model(
-    direct: bool = True,
-    normalize_images: bool = True,
-) -> FitModelReturn:
-    compiled_model = compile_model(
-        get_baseline_boiling_model(direct=direct, normalize_images=normalize_images),
-        get_baseline_compile_params(),
-    )
+# def get_pretrained_baseline_boiling_model(
+#     direct: bool = True,
+#     normalize_images: bool = True,
+# ) -> FitModelReturn:
+#     compiled_model = compile_model(
+#         get_baseline_boiling_model(direct=direct, normalize_images=normalize_images),
+#         get_baseline_compile_params(),
+#     )
 
-    return fit_boiling_model(
-        compiled_model,
-        baseline_boiling_dataset_direct if direct else baseline_boiling_dataset_indirect,
-        get_baseline_fit_params(),
-        target='Flux [W/cm**2]',
-    )
+#     return fit_boiling_model(
+#         compiled_model,
+#         baseline_boiling_dataset_direct if direct else baseline_boiling_dataset_indirect,
+#         get_baseline_fit_params(),
+#         target='Flux [W/cm**2]',
+#     )
