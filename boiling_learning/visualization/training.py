a8ee5c2ba830c419f673daaa4f9d6f500c3ebd55
Ruan Comelli <ruancomelli@gmail.com>
2022-12-04 10:56:42 -0300

    refactor(visualization/training): add helper constructor to `TrainingHistory` dataframe

--- boiling_learning/visualization/training.py
+++ boiling_learning/visualization/training.py
@@ -1,13 +1,38 @@
+from __future__ import annotations
+
 import matplotlib.pyplot as plt
 import pandera as pa
 from pandera.typing import DataFrame, Series
+
+from boiling_learning.model.training import FitModelReturn
 
 
 class TrainingHistory(pa.SchemaModel):
     epoch: Series[int] = pa.Field(ge=0)
     target: Series[float] = pa.Field()
 
+    @classmethod
+    def from_fit_model_return(
+        cls,
+        fit_model: FitModelReturn,
+        /,
+        *,
+        epoch_key: str = 'epoch',
+        target_key: str,
+    ) -> DataFrame[TrainingHistory]:
+        return DataFrame[TrainingHistory](
+            {
+                'epoch': entry[epoch_key],
+                'target': entry[target_key],
+            }
+            for entry in fit_model.history
+        )
+
 
 @pa.check_types
 def plot_training_history(history: DataFrame[TrainingHistory]) -> None:
-    plt.plot(history, x='epoch', y='target')
+    plt.plot(
+        history,
+        x=TrainingHistory.epoch,
+        y=TrainingHistory.target,
+    )
