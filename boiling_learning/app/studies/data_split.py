ee2b235db9f82fef028661ba45447e0ea20add2b
Ruan Comelli <ruancomelli@gmail.com>
2023-01-03 18:37:24 -0300

    feat(app/studies): add study about data splitting

---
+++ boiling_learning/app/studies/data_split.py
@@ -0,0 +1,84 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import rich
+import seaborn as sns
+import typer
+
+from boiling_learning.app.configuration import configure
+from boiling_learning.app.datasets.bridged.boiling1d import DEFAULT_BOILING_OUTLIER_FILTER
+from boiling_learning.app.datasets.preprocessed.boiling1d import baseline_boiling_dataset
+from boiling_learning.app.paths import studies_path
+from boiling_learning.app.training.boiling1d import DEFAULT_BOILING_HEAT_FLUX_TARGET
+from boiling_learning.datasets.sliceable import targets
+from boiling_learning.image_datasets import ImageDatasetTriplet
+from boiling_learning.lazy import LazyDescribed
+from boiling_learning.utils.pathutils import resolve
+
+app = typer.Typer()
+console = rich.console.Console()
+
+
+@app.command()
+def boiling1d() -> None:
+    configure(
+        force_gpu_allow_growth=True,
+        use_xla=True,
+        modin_engine='ray',
+        require_gpu=True,
+    )
+
+    datasets = baseline_boiling_dataset(
+        # use indirect visualization to save memory since images aren't really necessary
+        direct_visualization=False
+    )
+
+    data = _sorted_boiling_datasets(datasets)
+
+    f, ax = plt.subplots(1, 1, figsize=(6, 4))
+    sns.scatterplot(ax=ax, data=data, x='index', y='heat flux', hue='class', alpha=0.5)
+    f.savefig(str(_data_split_study_path() / 'boiling1d.png'))
+
+
+@app.command()
+def condensation(
+    each: int = typer.Option(60),
+    normalize: bool = typer.Option(...),
+) -> None:
+    raise NotImplementedError
+
+
+def _sorted_boiling_datasets(datasets: LazyDescribed[ImageDatasetTriplet]) -> pd.DataFrame:
+    ds_train, ds_val, ds_test = datasets()
+
+    df = pd.DataFrame(
+        sorted(
+            (
+                (
+                    target['nominal_power'],
+                    target[DEFAULT_BOILING_HEAT_FLUX_TARGET],
+                    target['elapsed_time'],
+                    class_name,
+                )
+                for class_name, ds in (
+                    ('train', ds_train),
+                    ('val', ds_val),
+                    ('test', ds_test),
+                )
+                for target in targets(ds).prefetch(1024)
+                if DEFAULT_BOILING_OUTLIER_FILTER()(None, target)
+            ),
+            key=lambda power_hf_et_class: (
+                power_hf_et_class[0],
+                power_hf_et_class[2],
+            ),
+        ),
+        columns=['nominal power', 'heat flux', 'elapsed time', 'class'],
+    )
+    df['index'] = range(len(df))
+    return df
+
+
+def _data_split_study_path() -> Path:
+    return resolve(studies_path() / 'data-split', dir=True)
