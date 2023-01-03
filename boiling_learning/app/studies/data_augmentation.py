c03d39e77ac52e127d56bf832e805283679b447f
Ruan Comelli <ruancomelli@gmail.com>
2023-01-03 18:37:01 -0300

    feat(app/studies): add data augmentation studies

---
+++ boiling_learning/app/studies/data_augmentation.py
@@ -0,0 +1,88 @@
+import rich
+import typer
+from rich.columns import Columns
+from rich.table import Table
+
+from boiling_learning.app.configuration import configure
+from boiling_learning.app.datasets.generators import get_image_dataset
+from boiling_learning.app.datasets.preprocessing import default_boiling_preprocessors
+from boiling_learning.app.datasets.raw.boiling1d import boiling_cases
+from boiling_learning.app.training.boiling1d import (
+    DEFAULT_BOILING_HEAT_FLUX_TARGET,
+    fit_boiling_model,
+)
+from boiling_learning.app.training.common import (
+    get_baseline_architecture,
+    get_baseline_compile_params,
+    get_baseline_fit_params,
+)
+from boiling_learning.model.training import compile_model
+
+app = typer.Typer()
+console = rich.console.Console()
+
+
+@app.command()
+def boiling1d() -> None:
+    """Validate current implementation against reference."""
+    strategy = configure(
+        force_gpu_allow_growth=True,
+        use_xla=True,
+        modin_engine='ray',
+        require_gpu=True,
+    )
+
+    case = boiling_cases()[0]
+
+    tables: list[Table] = []
+    for direct in False, True:
+        table = Table(
+            'Crop type',
+            'Validation loss',
+            'Test loss',
+            title=f'Random augmentation analysis - {"direct" if direct else "indirect"}',
+        )
+
+        for crop_mode in 'center', 'random':
+            preprocessors = default_boiling_preprocessors(
+                direct_visualization=direct,
+                crop_mode=crop_mode,
+            )
+            dataset = get_image_dataset(
+                case(),
+                transformers=preprocessors,
+                experiment='boiling1d',
+            )
+            compiled_model = get_baseline_architecture(
+                dataset,
+                normalize_images=True,
+                strategy=strategy,
+            ) | compile_model(
+                **get_baseline_compile_params(strategy=strategy),
+            )
+
+            fit_model = fit_boiling_model(
+                compiled_model,
+                dataset,
+                get_baseline_fit_params(),
+                target=DEFAULT_BOILING_HEAT_FLUX_TARGET,
+                strategy=strategy,
+            )
+
+            table.add_row(
+                str(crop_mode.title()),
+                str(fit_model.validation_metrics['MSE']),
+                str(fit_model.test_metrics['MSE']),
+            )
+
+        tables.append(table)
+
+    console.print(Columns(tables))
+
+
+@app.command()
+def condensation(
+    each: int = typer.Option(60),
+    normalize: bool = typer.Option(...),
+) -> None:
+    raise NotImplementedError
---
+++ boiling_learning/app/studies/data_augmentation_automl.py
@@ -0,0 +1,102 @@
+import typer
+from rich.columns import Columns
+from rich.console import Console
+from rich.table import Table
+
+from boiling_learning.app.automl.autofit_dataset import autofit_dataset
+from boiling_learning.app.configuration import configure
+from boiling_learning.app.datasets.generators import get_image_dataset
+from boiling_learning.app.datasets.preprocessing import default_boiling_preprocessors
+from boiling_learning.app.datasets.raw.boiling1d import boiling_cases
+from boiling_learning.app.training.boiling1d import (
+    DEFAULT_BOILING_HEAT_FLUX_TARGET,
+    get_pretrained_baseline_boiling_model,
+)
+from boiling_learning.app.training.common import get_baseline_compile_params
+from boiling_learning.app.training.evaluation import evaluate_boiling_model_with_dataset
+from boiling_learning.lazy import LazyDescribed
+from boiling_learning.model.training import compile_model
+
+app = typer.Typer()
+console = Console()
+
+
+@app.command()
+def boiling1d() -> None:
+    strategy = configure(
+        force_gpu_allow_growth=True,
+        use_xla=True,
+        modin_engine='ray',
+        require_gpu=True,
+    )
+
+    case = boiling_cases()[0]
+
+    tables: list[Table] = []
+    for direct in False, True:
+        table = Table(
+            'Crop mode',
+            'Validation loss',
+            'Test loss',
+            title=(
+                'Automatic machine learning - '
+                + ('direct' if direct else 'indirect')
+                + ' visualization'
+            ),
+        )
+        for crop_mode in 'center', 'random':
+            baseline_fit_return = get_pretrained_baseline_boiling_model(
+                direct_visualization=direct,
+                normalize_images=True,
+                strategy=strategy,
+            )
+
+            baseline_architecture_size = baseline_fit_return.architecture().count_parameters(
+                trainable=True,
+                non_trainable=False,
+            )
+
+            preprocessors = default_boiling_preprocessors(
+                direct_visualization=direct,
+                crop_mode=crop_mode,
+            )
+            datasets = get_image_dataset(
+                case(),
+                transformers=preprocessors,
+                experiment='boiling1d',
+            )
+
+            hypermodel = autofit_dataset(
+                datasets,
+                target=DEFAULT_BOILING_HEAT_FLUX_TARGET,
+                normalize_images=True,
+                max_model_size=baseline_architecture_size,
+                goal=None,
+                experiment='boiling1d',
+                strategy=strategy,
+            ).hypermodel
+
+            compiled_model = LazyDescribed.from_describable(
+                hypermodel.best_model()
+            ) | compile_model(
+                **get_baseline_compile_params(strategy=strategy),
+            )
+
+            _, validation_metrics, test_metrics = evaluate_boiling_model_with_dataset(
+                compiled_model,
+                datasets,
+            )
+
+            table.add_row(
+                crop_mode,
+                str(validation_metrics['MSE']),
+                str(test_metrics['MSE']),
+            )
+            tables.append(table)
+
+    console.print(Columns(tables))
+
+
+@app.command()
+def condensation() -> None:
+    raise NotImplementedError
