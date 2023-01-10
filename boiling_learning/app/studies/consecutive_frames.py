c65fdc3ad7d969c78cfd79eafac2f9a80a5b01cc
Ruan Comelli <ruancomelli@gmail.com>
2023-01-09 21:40:59 -0300

    fix(app/studies/consecutive_frames): expand analysis to cover all datasets

--- boiling_learning/app/studies/consecutive_frames.py
+++ boiling_learning/app/studies/consecutive_frames.py
@@ -6,37 +6,24 @@ import seaborn as sns
 import typer
 
 from boiling_learning.app.datasets.generators import get_image_dataset
+from boiling_learning.app.datasets.preprocessing import default_boiling_preprocessors
 from boiling_learning.app.datasets.raw.boiling1d import boiling_cases
 from boiling_learning.app.paths import studies_path
 from boiling_learning.datasets.sliceable import features
-from boiling_learning.preprocessing.image import (
-    VideoFrameOrFrames,
-    grayscaler,
-    image_dtype_converter,
-    structural_similarity,
-    structural_similarity_ratio,
-)
-from boiling_learning.preprocessing.transformers import Transformer
+from boiling_learning.preprocessing.image import structural_similarity_ratio
 from boiling_learning.utils.pathutils import resolve
 
 app = typer.Typer()
 
 
-METRICS = (
-    (structural_similarity, 'linear'),
-    (structural_similarity_ratio, 'linear'),
-)
-
 DATASET_MARKER_STYLE = (
-    ('Thick wire', 'k', 'o'),
-    ('Thin wire', 'r', '.'),
+    ('Large wire', 'k', 'o'),
+    ('Small wire', 'r', '.'),
     ('Horizontal ribbon', 'g', '>'),
     ('Vertical ribbon', 'b', '^'),
 )
 
-BOILING_PREPROCESSORS_COMPARE_INDEX = None
-
-DEFAULT_INDICES = list(range(1, 501))
+DEFAULT_INDICES = list(range(1, 11)) + list(range(10, 110, 10)) + [200, 300]
 
 
 @app.command()
@@ -45,53 +32,57 @@ def boiling1d(
     # indices: list[int] = typer.Option(list(range(1, 10)) + list(range(10, 100, 10))),
     indices: list[int] = typer.Option(DEFAULT_INDICES),
 ) -> None:
-    NROWS = len(METRICS)
     indices = sorted(frozenset(indices) | {0})
 
-    f, axes = plt.subplots(
-        NROWS,
+    f, ax = plt.subplots(
         1,
-        figsize=(6, NROWS * 4),
-        sharex='col',
+        1,
+        figsize=(6, 4),
     )
 
-    dataset = get_image_dataset(
-        boiling_cases()[0](),
-        transformers=_boiling_preprocessors(),
-        experiment='boiling1d',
-        shuffle=False,
-    )
-
-    ds_train, _, _ = dataset()
-    frames = features(ds_train).fetch(indices)
-    reference_frame = frames[0]
-
-    for row, (metric, xscale) in enumerate(METRICS):
-        metric_name = ' '.join(metric.__name__.split('_'))
-
-        ax = axes[row]
-        df = pd.DataFrame(
-            [(index, metric(reference_frame, frame)) for index, frame in zip(indices, frames)],
-            columns=['Index', metric_name],
+    metrics: list[tuple[int, float, str]] = []
+    for case, (dataset_name, _, _) in zip(boiling_cases(), DATASET_MARKER_STYLE):
+        preprocessors = [
+            default_boiling_preprocessors(
+                direct_visualization=False,
+                downscale_factor=1,
+            )[0]
+        ]
+        dataset = get_image_dataset(
+            case(),
+            transformers=preprocessors,
+            experiment='boiling1d',
+            shuffle=False,
         )
 
-        sns.scatterplot(ax=ax, data=df, x='Index', y=metric_name)
-        ax.set_xscale(xscale)
-        ax.set_yscale('linear')
-        ax.xaxis.grid(True, which='minor')
+        ds_train, _, _ = dataset()
+        frames = features(ds_train).fetch(indices)
+        reference_frame = frames[0]
+        metrics.extend(
+            [
+                (index, structural_similarity_ratio(reference_frame, frame), dataset_name)
+                for index, frame in zip(indices, frames)
+            ]
+        )
+
+    df = pd.DataFrame(
+        metrics,
+        columns=['Index', 'Structural similarity ratio', 'Dataset'],
+    )
+
+    sns.scatterplot(
+        ax=ax,
+        data=df,
+        x='Index',
+        y='Structural similarity ratio',
+        hue='Dataset',
+        markers=[marker for _, _, marker in DATASET_MARKER_STYLE],
+    )
+    ax.set_xscale('log')
+    ax.set_yscale('linear')
+    ax.xaxis.grid(True, which='minor')
 
     f.savefig(_consecutive_frames_study_path() / 'boiling1d.pdf')
-
-
-def _boiling_preprocessors() -> list[
-    list[
-        Transformer[VideoFrameOrFrames, VideoFrameOrFrames]
-        | dict[str, Transformer[VideoFrameOrFrames, VideoFrameOrFrames]]
-    ]
-]:
-    return [
-        [image_dtype_converter('float32'), grayscaler()],
-    ]
 
 
 @app.command()
