7835d33feb7c24c334f9f9a25d2b951c8fbebb37
Ruan Comelli <ruancomelli@gmail.com>
2023-01-21 23:00:43 -0300

    feat(app/displaying/figures): extract out dataset markers as a constant

--- boiling_learning/app/displaying/figures.py
+++ boiling_learning/app/displaying/figures.py
@@ -1,6 +1,15 @@
+from typing import Final
+
 from matplotlib.figure import Figure
 
 from boiling_learning.utils.pathutils import PathLike, resolve
+
+DATASET_MARKER_STYLE: Final = (
+    ('Large wire', 'o'),
+    ('Small wire', '.'),
+    ('Horizontal ribbon', '>'),
+    ('Vertical ribbon', '^'),
+)
 
 
 def save_figure(
