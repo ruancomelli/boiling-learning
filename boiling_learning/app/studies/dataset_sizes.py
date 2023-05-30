18bfd2f00ec3699d1109c203f0b1420e467ef05f
Ruan Comelli <ruancomelli@gmail.com>
2023-05-30 19:48:30 -0300

    feat(app/studies/dataset-sizes): also calculate the merged datasets sizes

--- boiling_learning/app/studies/dataset_sizes.py
+++ boiling_learning/app/studies/dataset_sizes.py
@@ -5,9 +5,15 @@ from rich.table import Table
 from boiling_learning.app.configuration import configure
 from boiling_learning.app.datasets.preprocessed.boiling1d import boiling_datasets
 from boiling_learning.app.datasets.preprocessed.condensation import condensation_dataset
+from boiling_learning.image_datasets import ImageDatasetTriplet
+from boiling_learning.lazy import LazyDescribed
+from boiling_learning.transforms import datasets_merger
 
 app = typer.Typer()
 console = Console()
+
+CASES_INDICES = ((0,), (1,), (2,), (3,), (0, 1), (2, 3), (0, 1, 2, 3))
+DIRECT_VISUALIZATION = False
 
 
 @app.command()
@@ -27,11 +33,17 @@ def boiling1d() -> None:
         title='Dataset sizes',
     )
 
-    for index, datasets in enumerate(boiling_datasets(direct_visualization=False), start=1):
+    all_boiling_datasets = boiling_datasets(direct_visualization=DIRECT_VISUALIZATION)
+    merged_datasets = {
+        indices: _build_merged_datasets(all_boiling_datasets, indices=indices)
+        for indices in CASES_INDICES
+    }
+
+    for indices, datasets in merged_datasets.items():
         train, val, test = datasets()
 
         table.add_row(
-            f'Dataset {index}',
+            f'Dataset {indices}',
             str(len(train)),
             str(len(val)),
             str(len(test)),
@@ -53,3 +65,17 @@ def condensation() -> None:
     console.print('Train set size:', len(train))
     console.print('Val set size:', len(val))
     console.print('Test set size:', len(test))
+
+
+def _build_merged_datasets(
+    datasets: tuple[LazyDescribed[ImageDatasetTriplet], ...],
+    /,
+    *,
+    indices: tuple[int, ...],
+) -> LazyDescribed[ImageDatasetTriplet]:
+    if len(indices) > 1:
+        selected_datasets = tuple(datasets[training_case] for training_case in indices)
+        return LazyDescribed.from_describable(selected_datasets) | datasets_merger()
+    else:
+        (index,) = indices
+        return datasets[index]
